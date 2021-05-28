c25e05c02763bbb4f2d4ea99dec944e16c06d4eb
Gustavo Brandão <56841881+brnd-21@users.noreply.github.com>
2021-05-27 21:25:44 -0300

    Update rps.js

--- comandos/jogos/rps.js
+++ comandos/jogos/rps.js
@@ -1,9 +1,13 @@
 module.exports = async ({ message, args }) => {
     let jooj = ["pedra", "papel", "tesoura", "pedra"];
     let player = Math.round(2 * Math.random())
+    
+    if(typeof args[0] != "undefined")
+        player = jooj.indexOf(args[0].toLowerCase())
+
     let bot = Math.round(2 * Math.random())
     let ganhador = "Perdeu!"
-
+    
     if (player == 0) player = 3
     if (bot == 0) bot = 3
 
