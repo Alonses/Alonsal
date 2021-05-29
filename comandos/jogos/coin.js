bad9464b52d46ff0e6ab82363802d84b4a22c17e
Gustavo Brandão <56841881+brnd-21@users.noreply.github.com>
2021-05-29 19:18:23 -0300

    Prevenção de erros

--- comandos/jogos/coin.js
+++ comandos/jogos/coin.js
@@ -2,15 +2,18 @@ module.exports = async ({ message, args }) => {
     
     var possibilidades = ["cara", "coroa"]
     var moeda = Math.round(1 * Math.random())
-    var escolha = args[0].toLowerCase();
-    
-    if (possibilidades.indexOf(args[0]) == -1) {
+    var escolha = "";
+
+    if(typeof args[0] != "undefined")
+        escolha = args[0].toLowerCase();
+
+    if(possibilidades.indexOf(escolha) == -1 || typeof args[0] == "undefined") {
         await message.channel.send('Informe cara ou coroa como `ãco cara` ou `ãco coroa` para testar sua sorte!')
         return
     }
-
-        if (escolha == possibilidades[moeda])
-            message.channel.send("[ :coin: ] Deu " + escolha + "! Você acertou!");
-        else
-            message.channel.send("[ :coin: ] Deu " + possibilidades[moeda] + ", perdeu playboy :v")
+    
+    if(escolha == possibilidades[moeda])
+        message.channel.send("[ :coin: ] Deu " + escolha + "! Você acertou!");
+    else
+        message.channel.send("[ :coin: ] Deu " + possibilidades[moeda] + ", perdeu playboy :v")
 }
