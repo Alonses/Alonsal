56f70ac51390107c4ba5b121efd9d1cc4d14fa01
Gustavo Brandão <56841881+brnd-21@users.noreply.github.com>
2021-05-30 13:56:54 -0300

    Novo comando

---
+++ comandos/manutencao/git.js
@@ -0,0 +1,4 @@
+module.exports = async function({message}) {
+
+    message.channel.send(`${message.author} a ceira mais recente está disponível por aqui :hot_face:` + "\nhttps://github.com/brnd-21/Alonsal");
+}
