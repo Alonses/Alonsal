9cff5e9964860aec8d3c95933f86acdc77965f29
Gustavo Brandão <56841881+brnd-21@users.noreply.github.com>
2021-05-31 13:42:52 -0300

    Prevenção de erros

--- comandos/diversao/curio.js
+++ comandos/diversao/curio.js
@@ -2,7 +2,7 @@ const curios = require('./curio.json');
 
 module.exports = async ({ message }) => {
 
-    var num = Math.round(110 * Math.random());
+    var num = 1 + Math.round(109 * Math.random());
 
     num = num.toString();
     message.channel.send(":clipboard: "+ curios[num]);
