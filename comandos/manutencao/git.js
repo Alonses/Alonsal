c9d93f4eb6fa82b64697c58b58469d5bb0e6440f
Gustavo Brandão <56841881+brnd-21@users.noreply.github.com>
2021-05-31 11:40:58 -0300

    Adição do Embed

--- comandos/manutencao/git.js
+++ comandos/manutencao/git.js
@@ -1,4 +1,14 @@
+const Discord = require('discord.js');
+
 module.exports = async function({message}) {
 
-    message.channel.send(`${message.author} a ceira mais recente está disponível por aqui :hot_face:` + "\nhttps://github.com/brnd-21/Alonsal");
+    const embed = new Discord.MessageEmbed()
+    .setColor(0x29BB8E)
+    .setAuthor('GitHub')
+    .setTitle("> Repositório Alonsal")
+    .setURL('https://github.com/brnd-21/Alonsal')
+    .setImage('https://i.imgur.com/0tV3IQr.png')
+    .setDescription("Contribua reportando bugs ou roubando códigos do Alonsal :pray_tone2:");
+
+    message.channel.send(`${message.author}`, embed);
 }
