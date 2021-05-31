56d87cb24f8847f5ded9cf5b1abaf95f9a9ed806
Gustavo Brandão <56841881+brnd-21@users.noreply.github.com>
2021-05-31 11:23:48 -0300

    Novo comando

---
+++ comandos/manutencao/convite.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+module.exports = async function({message}){
+
+    const embed = new Discord.MessageEmbed()
+    .setColor(0x29BB8E)
+    .setTitle("> Ó Abre alas que eu queru passsaa")
+    .setURL('https://discord.com/oauth2/authorize?client_id=833349943539531806&scope=bot&permissions=3202112')
+    .setThumbnail('https://i.imgur.com/K61ShGX.png')
+    .setDescription("Convide-me clicando aqui!")
+    .setTimestamp()
+    .setFooter("Alonsal");
+    
+    message.channel.send(`${message.author}`, embed);
+}
