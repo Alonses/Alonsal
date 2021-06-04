94b2b14634f81e955aec4b8371a5c164ac46fd3e
Gustavo Brandão <56841881+brnd-21@users.noreply.github.com>
2021-06-04 20:38:13 -0300

    Ajustes visuais

--- comandos/jogos/dado.js
+++ comandos/jogos/dado.js
@@ -1,5 +1,5 @@
 module.exports = async({message}) => {
     var dado = 1 + Math.round(5 * Math.random());
 
-    message.channel.send(`:game_die: ${message.author}, O dado caiu no [ `+ dado + ' ]');
+    message.channel.send(`:game_die: ${message.author}`+ ', O dado caiu no [ `'+ dado +'` ]');
 }
