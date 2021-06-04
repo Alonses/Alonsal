60e39d16f340045b2a4ba3e0442a1ebeccbaa36c
Gustavo Brandão <56841881+brnd-21@users.noreply.github.com>
2021-06-04 20:36:03 -0300

    Att json

--- comandos/diversao/curio.js
+++ comandos/diversao/curio.js
@@ -1,9 +1,12 @@
-const curios = require('./curio.json');
+const curios = require('./curio.json')
+const atach = require('./curio_atach.json')
 
 module.exports = async ({ message }) => {
 
-    var num = 1 + Math.round(109 * Math.random());
-
-    num = num.toString();
-    message.channel.send(":clipboard: "+ curios[num]);
+    var num = 1 + Math.round(138 * Math.random())
+    
+    message.channel.send(":clipboard: "+ curios[num])
+    
+    if(atach[num])
+        message.channel.send(atach[num])
 }
