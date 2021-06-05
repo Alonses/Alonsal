14aff46e15c0a47637dde84e02971a26cd84bd60
Gustavo <gusbra21@outlook.com>
2021-06-05 15:21:54 -0300

    ajustes

--- comandos/manutencao/convite.js
+++ comandos/manutencao/convite.js
@@ -4,7 +4,7 @@ module.exports = async function({message}){
 
     const embed = new Discord.MessageEmbed()
     .setColor(0x29BB8E)
-    .setTitle("> Ó Abre alas que eu queru passsaa")
+    .setTitle("> Ó Abre alas que eu queru passsaa :notes:")
     .setURL('https://discord.com/oauth2/authorize?client_id=833349943539531806&scope=bot&permissions=3202112')
     .setThumbnail('https://i.imgur.com/K61ShGX.png')
     .setDescription("Convide-me clicando aqui!")
