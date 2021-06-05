a19da1fd5d53564e81e98b89057a7f3daa869a0f
Gustavo Brandão <56841881+brnd-21@users.noreply.github.com>
2021-06-05 12:57:34 -0300

    Simplificação de funções

--- adm/status.js
+++ adm/status.js
@@ -1,4 +1,22 @@
-module.exports = sync function({client, message, args}){
+module.exports = sync function({client}){
 
+    console.log(`Caldeiras aquecidas!`);
+    console.log(`Ativo para ${client.users.cache.size} usuários em ${client.channels.cache.size} canais em ${client.guilds.cache.size} servidores diferentes!`);
 
+    client.user.setActivity('Vapor p/ fora!', 'COMPETING')
+    let activities = [
+        ".h | .help",
+        "Binário na fogueira",
+        "Músicas no ar",
+        "Mesas p/ cima",
+        "Baidu premium em servidores",
+        ".h | .help",
+        "Código morse para o mundo",
+        "Bugs infinitos no sistema",
+        "Vapor p/ fora!",
+        "Ceira para todo o lado"
+    ]
+    
+    i = 0;
+    setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`), 5000);
 }
