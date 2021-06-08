b159b90b258e44499c9f6fd89e94310dd07665a6
Gustavo <gusbra21@outlook.com>
2021-06-07 23:28:12 -0300

    Ajustes musicais

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ const ping_me_gif = require('./adm/ping_me.json');
 const client = new Discord.Client();
 
 prefix = config.prefix;
-usos = 1404;
-usos_anterior = 1404;
+usos = 1449;
+usos_anterior = 1449;
 local_server = config.log_servers;
 local_comando = config.log_commands;
 
