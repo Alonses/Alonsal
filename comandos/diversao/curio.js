57467544b7f4de9985790e9890cf90689db97663
lllggghhhaaa <lgha1423@outlook.com>
2021-06-12 05:58:39 +0300

    Enceirando curio.js

--- comandos/diversao/curio.js
+++ comandos/diversao/curio.js
@@ -1,12 +1,13 @@
-const curios = require('./curio.json')
-const atach = require('./curio_atach.json')
+const { messages } = require("../../arquivos/json/gifs/curio.json")
 
 module.exports = async ({ message }) => {
 
-    var num = 1 + Math.round(138 * Math.random())
+    let num = 1 + Math.round(messages.length * Math.random());
+
+    let key = Object.keys(messages[num]);
+
+    message.channel.send(":clipboard: "+ key);
     
-    message.channel.send(":clipboard: "+ curios[num])
-    
-    if(atach[num])
-        message.channel.send(atach[num])
+    if(messages[num][key] !== null)
+        message.channel.send(messages[num][key]);
 }
