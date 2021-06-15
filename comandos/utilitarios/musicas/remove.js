abfc526e69f10e9ca1090000282822522265c64a
Gustavo <gustavobrandao31@outlook.com>
2021-06-15 14:56:55 -0300

    Novas opções e ajustes

--- comandos/utilitarios/musicas/remove.js
+++ comandos/utilitarios/musicas/remove.js
@@ -12,16 +12,30 @@ module.exports = async function({message, args, playlists, nome_faixas, id_canal
         queue_local = playlists.get(id_canal)
         nome_faixas_l = nome_faixas.get(id_canal)
 
-        if(args[0] != 1){
-            // Removendo a faixa informada do array
-            link_removido = queue_local.splice(args[0] - 1, 1)
+        if(args[0] !== 1){
 
-            if(typeof nome_faixas_l != "undefined")
-                faixa_removida = nome_faixas_l.splice(args[0] - 1, 1)
-            else
-                faixa_removida = await ytdl.getInfo(link_removido).then(info => info.videoDetails.title);
+            if(args[0] === "all"){
+                let faixa_atual = queue_local[0];
+                let nome_faixa = nome_faixas_l[0];
 
-            message.channel.send(":wastebasket: A faixa [ `"+ faixa_removida +"` ] foi removida da playlist.")
+                queue_local = [];
+                nome_faixas_l = [];
+
+                queue_local.push(faixa_atual);
+                nome_faixas_l.push(nome_faixa);
+
+                message.channel.send(`:wastebasket: ${message.author} todas as próximas faixas foram removidas.`);
+            }else{
+                // Removendo a faixa informada do array
+                link_removido = queue_local.splice(args[0] - 1, 1)
+
+                if(typeof nome_faixas_l != "undefined")
+                    faixa_removida = nome_faixas_l.splice(args[0] - 1, 1)
+                else
+                    faixa_removida = await ytdl.getInfo(link_removido).then(info => info.videoDetails.title);
+
+                message.channel.send(":wastebasket: A faixa [ `"+ faixa_removida +"` ] foi removida da playlist.")
+            }
         }else{
             message.channel.send(`${message.author}` + "Use `.assk` para pular a faixa atual antes de remover ela")
             return
