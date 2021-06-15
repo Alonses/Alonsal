5ec75be566966ce8906fd4c1515b61e53d261b6e
Gustavo <gusbra21@outlook.com>
2021-06-15 01:38:41 -0300

    Prevenção a falhas, horario local e detalhes

--- comandos/utilitarios/tempo.js
+++ comandos/utilitarios/tempo.js
@@ -1,9 +1,12 @@
 const { MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
-const { api_key } = require('../../config.json')
+const { weather_key, time_key } = require('../../config.json')
 const { tempo_atual } = require('./tempo_atual.json')
 
-const base_url = "http://api.openweathermap.org/data/2.5/weather?"
+const base_url = "http://api.openweathermap.org/data/2.5/weather?";
+const time_url = "http://api.timezonedb.com/v2.1/get-time-zone?key=";
+
+const meses = ['', 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 
 module.exports = async({message, args}) => {
 
@@ -15,45 +18,62 @@ module.exports = async({message, args}) => {
     }
 
     for(let i = 0; i < args.length; i++){
-        pesquisa += args[i]
+        pesquisa += args[i].normalize("NFD").replace(/[^a-zA-Zs]/g, "");
 
         if(args[i + 1] !== undefined)
             pesquisa += " "
     }
 
-    let url_completa = base_url +"appid="+ api_key +"&q="+ pesquisa + "&units=metric";
-
+    let url_completa = base_url +"appid="+ weather_key +"&q="+ pesquisa + "&units=metric";
+    
     fetch(url_completa)
     .then(response => response.json())
     .then( async res => {
         if(res.cod == '404')
             message.channel.send(`${message.author} não encontrei nada relacionado a \``+ pesquisa +`\`, tente novamente`);
         else{
+            let url_hora = time_url +""+ time_key + "&format=json&by=position&lat="+ res.coord.lat +"&lng="+ res.coord.lon;
+            
+            fetch(url_hora) // Buscando o horário local
+            .then(response => response.json())
+            .then( async res_hora => {
+                let bandeira_pais = ' :flag_'+ (res.sys.country).toLowerCase() +':';
 
-            // let data = new Date()
+                let horario_local = res_hora.formatted;
+                horario_local = new Date(horario_local);
 
-            // data -= parseInt(res.timezone)
-            // data = Date(data)
+                let minutos = ("0" + horario_local.getMinutes()).substr(-2); // Preserva o digito 0
+                let hora = ("0" + horario_local.getHours()).substr(-2); // Preserva o digito 0
+                let dia = horario_local.getDate();
+                let mes = horario_local.getMonth() + 1;
 
-            // console.log(res)
-            // +'**\n:clock1230: **Hora local:** `'+ data +'`'
+                mes = meses[mes];
+                hours = horario_local.getHours();
 
-            let bandeira_pais = ' :flag_'+ (res.sys.country).toLowerCase() +':';
+                hours = hours % 12;
+                hours = hours ? hours : 12;
 
-            const cidade_encontrada = new MessageEmbed()
-            .setTitle(':boom: Tempo agora em '+ res.name + ' - '+ res.sys.country + ' '+ bandeira_pais)
-            .setColor(0x29BB8E)
-            .setDescription('**'+ tempo_atual[res.weather[0].id] + '**')
-            .setThumbnail('http://openweathermap.org/img/wn/'+ res.weather[0].icon +'@2x.png')
-            .addFields(
-                { name: ':thermometer: **Temperatura**', value: ":small_orange_diamond: **Atual**: `"+ res.main.temp +"°C`\n:small_red_triangle: **Máxima:** `"+ res.main.temp_max +"°C`\n:small_red_triangle_down: **Mínima:** `"+ res.main.temp_min +"°C`", inline: true },
-                { name: ':sweat_drops: **Umidade**', value: "**Atual: **`"+ res.main.humidity +"%`", inline: true },
-                { name: ':wind_chime: **Velocidade do vento**', value: " **Atual: **`"+ ( res.wind.speed * 3.6).toFixed(2) +" km/h`", inline: true },
-            )
-            .addField(':fire: **Sensação Térmica**', '**Atual: **`'+ res.main.feels_like +'°C`', true)
-            .addField(':compression: **Pressão do ar**', '**Atual: **`'+ res.main.pressure +' kPA`', true);
+                if(minutos >= 30)
+                    hours += "30"
+                
+                horario_local = hora +":"+ minutos +" | "+ dia +" de "+ mes;
+                let relogio_emoji = ":clock"+ hours +":";
 
-            message.channel.send(cidade_encontrada);
+                const cidade_encontrada = new MessageEmbed()
+                .setTitle(':boom: Tempo agora em '+ res.name + ' - '+ res.sys.country + ' '+ bandeira_pais)
+                .setColor(0x29BB8E)
+                .setDescription('**'+ tempo_atual[res.weather[0].id] +'** | '+ relogio_emoji +' **Hora local:** `'+ horario_local +'`')
+                .setThumbnail('http://openweathermap.org/img/wn/'+ res.weather[0].icon +'@2x.png')
+                .addFields(
+                    { name: ':thermometer: **Temperatura**', value: ":small_orange_diamond: **Atual**: `"+ res.main.temp +"°C`\n:small_red_triangle: **Máxima:** `"+ res.main.temp_max +"°C`\n:small_red_triangle_down: **Mínima:** `"+ res.main.temp_min +"°C`", inline: true },
+                    { name: ':sweat_drops: **Umidade**', value: "**Atual: **`"+ res.main.humidity +"%`", inline: true },
+                    { name: ':wind_chime: **Velocidade do vento**', value: " **Atual: **`"+ res.wind.speed +" km/h`", inline: true },
+                )
+                .addField(':fire: **Sensação Térmica**', '**Atual: **`'+ res.main.feels_like +'°C`', true)
+                .addField(':compression: **Pressão do ar**', '**Atual: **`'+ res.main.pressure +' kPA`', true);
+
+                message.channel.send(cidade_encontrada);
+            })
         }
     })
 }
