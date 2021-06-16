6c73a606c9556607c053c5e50c109fd701128c0b
Gustavo <gustavobrandao31@outlook.com>
2021-06-16 13:03:05 -0300

    Bug do embed

--- adm/status.js
+++ adm/status.js
@@ -1,5 +1,5 @@
 const { id_canais, version } = require('../config.json')
-const MessageEmbed = require('discord.js')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = async function({client}){
 
