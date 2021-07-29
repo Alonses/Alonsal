9aa57c2bcf10b8b27dcde32970494a3df9faf586
Gustavo <gustavobrandao31@outlook.com>
2021-07-29 00:35:55 -0300

    Ajustes e correções

--- comandos/utilitarios/ping.js
+++ comandos/utilitarios/ping.js
@@ -4,10 +4,32 @@ module.exports = {
     aliases: [ "" ],
     cooldown: 5,
     permissions: [ "SEND_MESSAGES" ],
-    execute(client, message, args) {
+    async execute(client, message, args) {
 
-        message.channel.send("O Ping está desabilitado por enquanto, mals por isso :thumbsup:");
-        // const m = message.channel.send("Ping?");
-        // m.edit(`Pong! Tá lagado um poco tô renderizando vídeo [ ${m.createdTimestamp - message.createdTimestamp}ms ].`);
+        const { emojis } = require('../../arquivos/json/text/emojis.json');
+        
+        function emoji(id){
+            return client.emojis.cache.get(id).toString();
+        }
+
+        emoji_barata = emoji(emojis.barata);
+        emoji_steve = emoji(emojis.steve_dancando);
+        emoji_pare = emoji(emojis.pare_agr);
+
+        const m = await message.channel.send("Ping?");
+        let delay = m.createdTimestamp - message.createdTimestamp;
+
+        let mensagem = ':ping_pong: Pong! [ **`'+ delay +'ms`** ] Tá lagado um poco tô renderizando vídeo '+ emoji_barata;
+        
+        if(delay < 200)
+            mensagem = ':ping_pong: Pong! [ **`'+ delay +'ms`** ] Tudo numa boa!';
+
+        if(delay < 100)
+            mensagem = ':ping_pong: Pong! [ **`'+ delay +'ms`** ] Dá até para jogar de olhos fechados! '+ emoji_steve;
+
+        if(delay > 600)
+            mensagem = ':ping_pong: Pong! [ **`'+ delay +'ms`** ] Pode jogar fora esse teu roteador! '+ emoji_pare;
+
+        m.edit(mensagem);
     }
 };
