b58f89b10b082bc28db08e1018aa94e86089e25e
Gustavo <gustavobrandao31@outlook.com>
2021-08-01 17:54:47 -0300

    Correções e ajustes

--- comandos/utilitarios/avatar.js
+++ comandos/utilitarios/avatar.js
@@ -9,13 +9,20 @@ module.exports = {
         const fetch = require('node-fetch');
 
         const { MessageEmbed } = require("discord.js")
-        
-        if(!isNaN(args[0])) // Coleta por ID numérico no chat
-            user = client.users.cache.get(args[0]);
-        else
+        const { emojis_negativos } = require('../../arquivos/json/text/emojis.json');
+        let emoji_nao_encontrado = client.emojis.cache.get(emojis_negativos[Math.round((emojis_negativos.length - 1) * Math.random())]).toString();
+
+        if(!isNaN(args[0])){ // Coleta por ID numérico no chat
+            try{
+                user = message.guild.members.cache.get(args[0]).user;
+            }catch(err){
+                message.lineReply(emoji_nao_encontrado + ' | Eu não conheço este usuário, use o comando marcando ele! :smiling_imp:');
+                return;
+            }
+        }else
             user = message.mentions.users.first() || message.author;
 
-        let avatar = user.displayAvatarURL({ size: 512 }); 
+        let avatar = user.displayAvatarURL({ size: 2048 }); 
         
         url = 'https://cdn.discordapp.com/avatars/'+ user.id +'/'+ user.avatar +'.gif?size=512';
         avatar = url;
