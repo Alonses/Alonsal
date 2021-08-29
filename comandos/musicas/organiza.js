0be51fc5a3d3ba92695f1426c6bd663159dc969f
Gustavo <gustavobrandao31@outlook.com>
2021-08-29 15:30:18 -0300

    Correções musicais

--- comandos/musicas/organiza.js
+++ comandos/musicas/organiza.js
@@ -4,7 +4,9 @@ module.exports = async function({client, message, args, playlists, nome_faixas, 
     feedback = feedback_faixa.get(id_canal);
     queue_local = playlists.get(id_canal);
 
-    if(message.content === ".asrs" && message.author.id === "665002572926681128"){
+    console.log(message.content);
+
+    if(message.content === ".asrs"){
         
         await playlists.set(id_canal, []);
         await nome_faixas.set(id_canal, []);
@@ -17,12 +19,18 @@ module.exports = async function({client, message, args, playlists, nome_faixas, 
     }
     
     if(message.content === ".asrp"){
+
+        if(typeof queue_local == "undefined")
+            queue_local = [];
+
         if(repeteco_ === 0){
             await repeteco.set(id_canal, 1);
             message.lineReply(":repeat: Repeteco ativado, use `.asrp` novamente para desativar.");
 
-            if(queue_local.length < 6)
+            if(queue_local.length < 6 && queue_local.length != 0)
                 message.lineReply("Playlist pequena, irei parar de falar o nome das músicas quando elas iniciarem :man_detective:");
+            else if(queue_local.length == 0)
+                message.lineReply("Sem faixas na playlist, insira algumas pesquisando ou usando o random");
             else
                 message.lineReply("Use `.asfd` para desligar o anúncio de novas faixas :thumbsup:");
         }else{
@@ -43,6 +51,8 @@ module.exports = async function({client, message, args, playlists, nome_faixas, 
             msg = "Ok, sem anúncios de faixas que começarem a tocar";
         }
         
+        tocar(message, client, args, playlists, nome_faixas, atividade_bot, repeteco, feedback_faixa, "updt");
+
         message.lineReply(msg);
         return;
     }
