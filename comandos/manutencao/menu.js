dc80e2da2e40802a7e790dd0c0f3d78fcf23d0f7
Gustavo <gusbra21@outlook.com>
2021-10-13 18:12:33 -0300

    Correções 2.0

--- comandos/manutencao/menu.js
+++ comandos/manutencao/menu.js
@@ -25,9 +25,7 @@ module.exports = {
         let emoji_dancando = client.emojis.cache.get(emojis_dancantes[Math.round((emojis_dancantes.length - 1) * Math.random())]).toString();
 
         let prefix = client.prefixManager.getPrefix(message.guild.id);
-        if(!prefix)
-            prefix = ".a";
-
+        
         let embed_inicial,
             embed_diversao,
             embed_utilitarios,
