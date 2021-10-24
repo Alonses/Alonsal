3258e0c1fcbc0fcf4c8eb08299eeee04129bfba7
Gustavo <gusbra21@outlook.com>
2021-10-23 22:53:37 -0300

    Correção de erros

--- comandos/moderacao/prefixo.js
+++ comandos/moderacao/prefixo.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "prefixo",
-    description: "alterar o prefixo do alonsal",
+    description: "Altere o prefixo do alonsal",
     cooldown: 5,
     aliases: [ "setprefix", "prefix", "px" ],
     permissions: [ "SEND_MESSAGES" ],
@@ -8,7 +8,7 @@ module.exports = {
 
         const { moderacao } = require('../../arquivos/idiomas/'+ client.idioma.getLang(message.guild.id) +'.json');
         
-        if(!message.member.permissions.has("ADMINISTRATOR") && !client.owners.contains(message.author.id)) return message.reply(":octagonal_sign: | "+ moderacao[5]["moderadores"]); // Libera configuração para o proprietários e adms apenas
+        if(!message.member.permissions.has("ADMINISTRATOR") && !client.owners.includes(message.author.id)) return message.reply(":octagonal_sign: | "+ moderacao[5]["moderadores"]); // Libera configuração para o proprietários e adms apenas
 
         if(args.length !== 1)
             return message.reply(":interrobang: | "+ moderacao[5]["error_1"] +" `+px`").then(msg => setTimeout(() => msg.delete(), 5000));
