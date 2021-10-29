1efe9cf3fd3e5e05cc003f9f2cd688ebe7dce720
Gustavo <gustavobrandao31@outlook.com>
2021-10-29 13:11:07 -0300

    Ajustes e OCR (beta)

---
+++ comandos/utilitarios/ocr.js
@@ -0,0 +1,32 @@
+const { createWorker } = require('tesseract.js');
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "ocr",
+    description: "converte o texto de uma imagem em string",
+    aliases: [ "" ],
+    cooldown: 5,
+    permissions: [ "SEND_MESSAGES" ],
+    async execute(client, message, args){
+
+        if(message.attachments.size != 1) return message.reply(":warning: | Inclua uma imagem junto do comando");
+
+        let imagem = message.attachments.first();
+        imagem = imagem.url;
+        
+        const worker = createWorker();
+
+        await worker.load();
+        await worker.loadLanguage('por');
+        await worker.initialize('por');
+        const { data: { text } } = await worker.recognize(imagem);
+        
+        const texto_formatado = new MessageEmbed()
+        .setTitle("> :newspaper2: Seu OCR")
+        .setColor(0x29BB8E)
+        .setDescription(`\`${text}\``)
+
+        message.reply({ embeds: [texto_formatado] });
+        await worker.terminate();
+    }
+}
