dc98445d9aa3e17fb7d0fbe4454a76d31eeeae27
Gustavo <gusbra21@outlook.com>
2021-10-29 13:11:07 -0300

    Ajustes e OCR (beta)

--- comandos/utilitarios/calculadora.js
+++ comandos/utilitarios/calculadora.js
@@ -10,42 +10,25 @@ module.exports = {
 
         const {utilitarios} = require('../../arquivos/idiomas/' + client.idioma.getLang(message.guild.id) + '.json');
 
-        if(args.length < 3 && !args[0].includes("!")){
-            if(args[0].includes("!")){
+        if(args.length < 1) return message.reply(utilitarios[17]["aviso_1"]);
+        
+        let operacao = "";
 
-                let fatorial = 1;
-                let num = parseInt(args[0].replace("!", ""));
+        try{
+            args.forEach(value => {
+                operacao += value +" ";
+            });
 
-                for(let i = 1; i < num + 1; i++){
-                    fatorial *= i;
-                }
+            operacao = operacao.slice(0, -1);
+            let resultado = math.evaluate(operacao);
+            let emoji_res = ":chart_with_upwards_trend:";
 
-                return message.reply(`${utilitarios[17]["resultado"]}: \`${fatorial}\``);;
-            }else
-                return message.reply(utilitarios[17]["aviso_1"]);
-        }else{
-
-            let operacao = "";
-
-            try{
-                args.forEach(value => {
-                    operacao += value +" ";
-                });
-
-                operacao = operacao.slice(0, -1);
-                let resultado = math.eval(operacao);
-                let emoji_res =  ":chart_with_upwards_trend:";
-
-                if(resultado < 0)
-                    emoji_res = ":chart_with_downwards_trend:";
-
-                if(operacao === "2 - 1" || operacao === "1 - 2")
-                    return message.reply(`:chart_with_upwards_trend: | ${utilitarios[17]["resultado"]}: \`3\``);
-
-                message.reply(`${emoji_res} | ${utilitarios[17]["resultado"]}: \`${resultado}\``);
-            }catch(err){
-                message.reply(`:octagonal_sign: | ${utilitarios[17]["error"]}: \`${operacao}\``);
-            }
+            if(resultado < 0)
+                emoji_res = ":chart_with_downwards_trend:";
+            
+            message.reply(`${emoji_res} | ${utilitarios[17]["resultado"]}: \`${resultado}\``);
+        }catch(err){
+            message.reply(`:octagonal_sign: | ${utilitarios[17]["error"]}: \`${operacao}\``);
         }
     }
 }
