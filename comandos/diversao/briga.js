75cf057b3d03ab9a1dca98413dfbd1652933063c
Gustavo <gusbra21@outlook.com>
2021-11-03 22:43:26 -0300

    Ajustes e comandos clássicos

---
+++ comandos/diversao/briga.js
@@ -0,0 +1,23 @@
+const { gifs } = require("../../arquivos/json/gifs/briga.json");
+
+module.exports = {
+    name: "briga",
+    description: "Porradaria!",
+    aliases: [ "b" ],
+    cooldown: 5,
+    permissions: [ "SEND_MESSAGES" ],
+    execute(client, message, args) {
+        
+        const num = Math.round((gifs.length - 1) * Math.random());
+        
+        if(num === 0)
+            message.channel.send("ESFIHADA!");
+
+        message.channel.send(gifs[num]);
+
+        const permissions = message.channel.permissionsFor(message.client.user);
+        
+        if(permissions.has("MANAGE_MESSAGES")) // Permissão para gerenciar mensagens
+            message.delete();
+    }
+};
