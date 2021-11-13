62429c1bb1cd220086f031ef889592022b35e3a5
Gustavo <gustavobrandao31@outlook.com>
2021-11-13 00:00:09 -0300

    Melhorias na calculadora

--- comandos/utilitarios/calculadora.js
+++ comandos/utilitarios/calculadora.js
@@ -1,3 +1,4 @@
+const { isInteger } = require('mathjs');
 const math = require('mathjs');
 
 module.exports = {
@@ -12,14 +13,24 @@ module.exports = {
 
         if(args.length < 1) return message.reply(utilitarios[17]["aviso_1"]);
         
+        let operacao = args.join(" ");
+        
+        if(args[0] === "+" || args[0] === "-" || args[0] === "*" || args[0] === "/"){
+            let operador = args[0];
+            args.shift();
+            operacao = args.join(` ${operador} `);
+        }
+
         try{
-            let operacao = args.join(" ");
             let resultado = math.evaluate(operacao);
             let emoji_res = ":chart_with_upwards_trend:";
 
             if(resultado < 0)
                 emoji_res = ":chart_with_downwards_trend:";
             
+            if(!isInteger(resultado))
+                resultado = resultado.toFixed(6)
+
             message.reply(`${emoji_res} | ${utilitarios[17]["resultado"]}: \`${resultado}\``);
         }catch(err){
             message.reply(`:octagonal_sign: | ${utilitarios[17]["error"]}: \`${operacao}\``);
