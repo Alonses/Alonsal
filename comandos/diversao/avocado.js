8d742314fb66060dbf2c40eec50e3aa3cc2894cf
Gustavo <gustavobrandao31@outlook.com>
2021-11-27 23:29:13 -0300

    Avocado

---
+++ comandos/diversao/avocado.js
@@ -0,0 +1,18 @@
+const { gifs } = require("../../arquivos/json/gifs/avocado.json");
+
+module.exports = {
+    name: "avocado",
+    description: "Nicokado avocado",
+    aliases: [ "vocado", "waterweight", "fault" ],
+    cooldown: 5,
+    permissions: [ "SEND_MESSAGES" ],
+    execute(client, message, args){
+        
+        message.channel.send(gifs[Math.round((gifs.length - 1) * Math.random())]).then(() => {
+            const permissions = message.channel.permissionsFor(message.client.user);
+
+            if(permissions.has("MANAGE_MESSAGES")) // Permissão para gerenciar mensagens
+                message.delete();
+        });
+    }
+}
