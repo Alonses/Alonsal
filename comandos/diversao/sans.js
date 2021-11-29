24b30e58702190e1073314fcdeca467d1ee1e100
Gustavo <gusbra21@outlook.com>
2021-11-28 22:04:14 -0300

    Correção de erros

--- comandos/diversao/sans.js
+++ comandos/diversao/sans.js
@@ -6,13 +6,18 @@ module.exports = {
     permissions: [ "SEND_MESSAGES" ],
     execute(client, message, args){
         
-        if(args.length < 1) return message.reply("Escreva algo");
+        const { diversao } = require(`../../arquivos/idiomas/${client.idioma.getLang(message.guild.id)}.json`);
+        const prefix = client.prefixManager.getPrefix(message.guild.id);
+
+        if(args.length < 1) return message.reply(diversao[7]["aviso_1"].replace(".a", prefix));
         let texto = args.join(" ").split("");
         
         for(let i = 0; i < texto.length; i++)
             if(i % 2 === 0 && i % 1 === 0)
                 texto[i] = texto[i].toLocaleUpperCase();
+            else
+                texto[i] = texto[i].toLocaleLowerCase();
 
-        message.reply(texto.join(""));
+        message.reply(`\`\`\`${texto.join("").slice(0, 1990)}\`\`\``);
     }
 }
