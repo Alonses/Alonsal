3b0fa15a2c0d535dac0161a48970c50e947db6d2
Gustavo <gusbra21@outlook.com>
2021-12-04 21:23:03 -0300

    correções

--- adm/internos/status.js
+++ adm/internos/status.js
@@ -3,15 +3,16 @@ const { MessageEmbed } = require('discord.js')
 
 module.exports = async ({client}) => {
     
-    const canais_texto = client.channels.cache.filter((c) => c.type === "GUILD_TEXT").size;
-    const canais_voz = client.channels.cache.filter((c) => c.type === "GUILD_VOICE").size;
-    let members = 0;
-
-    client.guilds.cache.forEach(async guild => {
-        members += guild.memberCount - 1;
-    });
-
     if(client.user.id === "833349943539531806"){
+
+        const canais_texto = client.channels.cache.filter((c) => c.type === "GUILD_TEXT").size;
+        const canais_voz = client.channels.cache.filter((c) => c.type === "GUILD_VOICE").size;
+        let members = 0;
+
+        client.guilds.cache.forEach(async guild => {
+            members += guild.memberCount - 1;
+        });
+
         const embed = new MessageEmbed()
         .setTitle(':steam_locomotive: Caldeiras aquecidas')
         .setColor(0x29BB8E)
@@ -36,19 +37,20 @@ module.exports = async ({client}) => {
         .setFooter("Alonsal", "https://i.imgur.com/K61ShGX.png");
         
         client.channels.cache.get('854695578372800552').send({ embeds: [embed] }); // Avisa que está online em um canal
-    }
+            
+        client.user.setActivity('Vapor p/ fora!', 'COMPETING')
+        const activities = [
+            "Rank 5X!!!!!",
+            "🎄Boas Festas!",
+            ".alang pt | .alang en",
+            `Neve em ${client.guilds.cache.size} servidores`,
+            "🎅Seria santa santa seria?",
+            "Ping me for prefix!",
+            "❄️Let's snow Let's snow Let's snow...",
+        ];
 
-    client.user.setActivity('Vapor p/ fora!', 'COMPETING')
-    const activities = [
-        "Rank 5X!!!!!",
-        "🎄Boas Festas!",
-        ".alang pt | .alang en",
-        `Neve em ${client.guilds.cache.size} servidores`,
-        "🎅Seria santa santa seria?",
-        "Ping me for prefix!",
-        "❄️Let's snow Let's snow Let's snow...",
-    ];
-
-    let i = 0;
-    setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`), 10000);
+        let i = 0;
+        setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`), 10000);
+    }else
+        client.user.setPresence({ activities: [{ name: 'baidu nos servidores' }] });        
 }
--- comandos/utilitarios/channelinfo.js
+++ comandos/utilitarios/channelinfo.js
@@ -5,7 +5,7 @@ module.exports = {
     name: "channelinfo",
     description: "Veja detalhes de algum canal",
     aliases: [ "chinfo", "cinfo" ],
-    cooldown: 1,
+    cooldown: 3,
     permissions: [ "SEND_MESSAGES" ],
     async execute(client, message, args) {
 
@@ -16,18 +16,18 @@ module.exports = {
 
         let canal = message.channel;
         
-        if(typeof args[0] !== "undefined"){
-            if(args[0].includes("<#")){
-                args[0] = args[0].replace("<#", "");
-                args[0] = args[0].replace(">", "");
-            }
+        if(typeof args[0] !== "undefined"){ // ID's customizados para canais aleatórios, marcando ou não
+            let id_canal = args[0].toString()
 
-            if(isNaN(args[0])) return message.reply("Informe o ID de um canal!");
+            if(id_canal.includes("<#"))
+                id_canal = id_canal.replace("<#", "").replace(">", "");
+            
+            if(isNaN(id_canal)) return message.reply(utilitarios[15]["aviso_1"]);
 
             try{
-                canal = await client.channels.fetch(args[0]);
+                canal = await client.channels.fetch(id_canal); // Coletando os dados do canal informado
             }catch(err){
-                return message.reply("Não foi possível localizar este canal, tente novamente");
+                return message.reply(`:octagonal_sign: | ${utilitarios[15]["error_1"]}`);
             }
         }
 
@@ -36,13 +36,13 @@ module.exports = {
             nsfw = utilitarios[9]["sim"];
         
         const data_atual = new Date();
-        let data_criacao = formata_data(new Date(canal.createdAt), idioma_selecionado); // Criação do servidor
+        let data_criacao = formata_data(new Date(canal.createdAt), idioma_selecionado); // Criação do canal
         let diferenca_criacao = getDateDiff(new Date(canal.createdAt), data_atual, utilitarios);
         let userlimit, bitrate = "";
 
         let topico = `\`\`\`${canal.topic}\`\`\``;
         if(!canal.topic)
-            topico = utilitarios[15]["sem_topico"];
+            topico = `\`\`\`${utilitarios[15]["sem_topico"]}\`\`\``;
         
         if(typeof canal.bitrate !== "undefined"){
             topico = `\`\`\`🔊 ${utilitarios[15]["canal_voz"]}\`\`\``;
@@ -68,33 +68,55 @@ module.exports = {
             .setColor(0x29BB8E)
             .setDescription(topico)
             .addFields(
-                { name: `:globe_with_meridians: **${utilitarios[15]["id_canal"]}**`, value: `\`${canal.id}\``, inline: true },
-                { name: `:label: **${utilitarios[15]["mencao"]}**`, value: `\`<#${canal.id}>\``, inline: true },
+                { 
+                    name: `:globe_with_meridians: **${utilitarios[15]["id_canal"]}**`,
+                    value: `\`${canal.id}\``,
+                    inline: true
+                },
+                { 
+                    name: `:label: **${utilitarios[15]["mencao"]}**`,
+                    value: `\`<#${canal.id}>\``,
+                    inline: true
+                },
             );
 
             if(bitrate === "")
                 infos_ch.addFields(
-                    { name: ':underage: NSFW', value: `\`${nsfw}\``, inline: true}
+                    { 
+                        name: ':underage: NSFW',
+                        value: `\`${nsfw}\``,
+                        inline: true
+                    }
                 )
             else
-                infos_ch.addFields(
-                    { name: '⠀', value: `⠀`, inline: true}
-                )
+                infos_ch.addFields({  name: '⠀', value: '⠀', inline: true })
 
             infos_ch.addFields(
-                { name: `:birthday: ${utilitarios[12]["criacao"]}`, value: `\`${data_criacao}\`\n [ \`${diferenca_criacao}\` ]`, inline: true}
+                { 
+                    name: `:birthday: ${utilitarios[12]["criacao"]}`,
+                    value: `\`${data_criacao}\`\n [ \`${diferenca_criacao}\` ]`,
+                    inline: true
+                }
             )
             .setFooter(`${utilitarios[15]["servidor"]}: ${canal.guild.name}`, message.author.avatarURL({ dynamic:true }));
             
             if(typeof canal.bitrate !== "undefined")
                 infos_ch.addFields(
-                    { name: `:mega: ${utilitarios[15]["transmissao"]}`, value: `:radio: **Bitrate: **\`${bitrate}\`\n:busts_in_silhouette: **Max. users: **\`${userlimit}\``, inline: true}
+                    { 
+                        name: `:mega: ${utilitarios[15]["transmissao"]}`,
+                        value: `:radio: **Bitrate: **\`${bitrate}\`\n:busts_in_silhouette: **Max. users: **\`${userlimit}\``,
+                        inline: true
+                    }
                 )
 
             if(typeof canal.rateLimitPerUser !== "undefined")
                 if(canal.rateLimitPerUser > 0)
                     infos_ch.addFields(
-                        { name: `:name_badge: ${utilitarios[15]["modo_lento"]}`, value: `\`${canal.rateLimitPerUser} segundos\``, inline: true}
+                        { 
+                            name: `:name_badge: ${utilitarios[15]["modo_lento"]}`,
+                            value: `\`${canal.rateLimitPerUser} segundos\``,
+                            inline: true
+                        }
                     )
             
             return message.reply({embeds: [infos_ch]});
