7f6655e6b89f7321bbfa503df8fb974ef70945cf
Gustavo <gusbra21@outlook.com>
2022-01-28 21:49:56 -0300

    rastreando bugs

--- comandos/diversao/piadas.js
+++ comandos/diversao/piadas.js
@@ -18,21 +18,21 @@ module.exports = {
         lista.push(alvo);
                 
         message.reply(`:black_joker: | ${piadas[alvo]}`);
-    },
-    slash_params: [{
-        name: "Piadas",
-        description: "Piadocas aleatórias"
-    }],
-    slash(client, handler, data, params) {
-
-        if (lista.length === piadas.length) lista = [];
-
-        do {
-            alvo = Math.round((piadas.length - 1) * Math.random());
-        } while(lista.includes(alvo));
-
-        lista.push(alvo);
-        
-        handler.postSlashMessage(data, `:black_joker: | ${piadas[alvo]}`);
     }
+    // slash_params: [{
+    //     name: "Piadas",
+    //     description: "Piadocas aleatórias"
+    // }],
+    // slash(client, handler, data, params) {
+
+    //     if (lista.length === piadas.length) lista = [];
+
+    //     do {
+    //         alvo = Math.round((piadas.length - 1) * Math.random());
+    //     } while(lista.includes(alvo));
+
+    //     lista.push(alvo);
+        
+    //     handler.postSlashMessage(data, `:black_joker: | ${piadas[alvo]}`);
+    // }
 };
