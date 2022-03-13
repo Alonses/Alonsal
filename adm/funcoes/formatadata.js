385c94aa9b59aafa3c56a76b465f9f9b59ca82a6
Gustavo <gusbra21@outlook.com>
2022-03-13 17:34:11 -0300

    French 2.0

--- adm/funcoes/formatadata.js
+++ adm/funcoes/formatadata.js
@@ -4,6 +4,8 @@ module.exports = (data_informada, idioma_selecionado) => {
 
     if (idioma_selecionado === "pt-br")
         return `${data_informada.getDate()} de ${data_informada.toLocaleString('pt', {month: 'long'})} de ${data_informada.getFullYear()} às ${("0" + data_informada.getHours()).substr(-2) + ":" + ("0" + data_informada.getMinutes()).substr(-2)} ${turno_horario}`;
-    else
+    else if(idioma_selecionado == "en-us")
         return `${data_informada.toLocaleString('en', {month: 'long'})} ${data_informada.getDate()}, ${data_informada.getFullYear()} at ${("0" + data_informada.getHours()).substr(-2)}:${("0" + data_informada.getMinutes()).substr(-2)} ${turno_horario}`;
+    else
+        return `${data_informada.toLocaleString('fr', {month: 'long'})} ${data_informada.getDate()}, ${data_informada.getFullYear()} et ${("0" + data_informada.getHours()).substr(-2)}:${("0" + data_informada.getMinutes()).substr(-2)} ${turno_horario}`;
 }
