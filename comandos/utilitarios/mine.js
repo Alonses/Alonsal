2900b80958611f57879da07ab3882abcdb3a0115
Gustavo <gustavobrandao31@outlook.com>
2022-06-17 23:57:01 -0300

    Wiki do minecraft e melhorias no .amc

--- comandos/utilitarios/mine.js
+++ comandos/utilitarios/mine.js
@@ -1,6 +1,7 @@
 const { emojis, emojis_negativos } = require('../../arquivos/json/text/emojis.json');
 const { MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
+const fs = require('fs');
 
 module.exports = {
     name: "mine",
@@ -11,51 +12,69 @@ module.exports = {
     execute(client, message, args) {
         const idioma_selecionado = client.idioma.getLang(message.guild.id);
         const { utilitarios } = require(`../../arquivos/idiomas/${idioma_selecionado}.json`);
+        const prefix = client.prefixManager.getPrefix(message.guild.id);
 
-        let nota_rodape = "";
+        let nota_rodape = (utilitarios[9]["sugestao_pesquisa"]).replace(".a", prefix), objeto_encontrado = false, i = 0;
         const emoji_suv = client.emojis.cache.get(emojis.mc_coracao).toString();
+        const logo_wikipedia = client.emojis.cache.get(emojis.mc_logo_wikipedia).toString();
 
-        let pesquisa = args.join(" ");
-        const nome_interno = pesquisa.split(" ").join("_").toLocaleLowerCase(); // Pesquisa usando nome em inglês/interno
-        pesquisa = pesquisa.charAt(0).toUpperCase() + pesquisa.slice(1);
+        let pesquisa_crua = args.join(" ");
+        const nome_interno = pesquisa_crua.split(" ").join("_").toLocaleLowerCase(); // Pesquisa usando nome em inglês/interno
+        pesquisa_crua = pesquisa_crua.charAt(0).toUpperCase() + pesquisa_crua.slice(1);
 
-        const random = pesquisa === "";
+        const random = pesquisa_crua === "";
 
         fetch('https://raw.githubusercontent.com/odnols/inventario-mine/main/Files/JSON/dados_locais.json')
         .then(response => response.json())
         .then(async lista_itens => {
-            let descr_pesquisa;
-            let url;
-            let alvo_json;
-            let alvo;
-            for (let i = 0; i < lista_itens.length; i++) {
+            let descr_pesquisa, url, alvo, alvo_json, pesquisa;
+
+            while (i < lista_itens.length && !objeto_encontrado) {
 
                 let descri = false;
-
                 const nome_simplificado = lista_itens[i].nome_item.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLocaleLowerCase();
-
+                
                 let auto_compl = nome_simplificado;
                 auto_compl = auto_compl.toLocaleLowerCase();
 
                 if (lista_itens[i].descricao !== null) {
                     descr_pesquisa = lista_itens[i].descricao.toLocaleLowerCase();
 
-                    if (descr_pesquisa.includes(pesquisa.toLocaleLowerCase()))
+                    if (descr_pesquisa.includes(pesquisa_crua.toLocaleLowerCase()))
                         descri = true;
                 }
 
-                if ((pesquisa === nome_simplificado || pesquisa === lista_itens[i].nome_interno) || random || nome_interno === lista_itens[i].nome_interno || descri || auto_compl.includes(pesquisa.toLocaleLowerCase())) {
+                // Responsável pelo auto completa da pesquisa
+                let nome_simplificado_verif = false;
+                let nome_interno_verif = false;
+                let auto_compl_verif = false;
+                
+                if(!pesquisa_crua.includes("\"")){ // Verificando se não é uma pesquisa bruta
+                    auto_compl_verif = auto_compl.includes(pesquisa_crua.toLocaleLowerCase());
+                    nome_simplificado_verif = pesquisa_crua === nome_simplificado;
+                    nome_interno_verif = pesquisa_crua === lista_itens[i].nome_interno;
 
+                    pesquisa = pesquisa_crua;
+                }else{ // Pesquisa bruta
+                    pesquisa = pesquisa_crua.replaceAll("\"", "");
+                    auto_compl_verif = (pesquisa.length === nome_simplificado.length) && (pesquisa === nome_simplificado);
+                }
+
+                if ((nome_simplificado_verif || nome_interno_verif) || random || nome_interno === lista_itens[i].nome_interno || descri || auto_compl_verif) {
+                    
                     if (random)
                         i = Math.round((lista_itens.length - 1) * Math.random());
 
-                    url = `https://raw.githubusercontent.com/brnd-21/inventario-mine/main/IMG/Itens/new/${lista_itens[i].tipo_item}/${lista_itens[i].nome_icon}`;
+                    url = `https://raw.githubusercontent.com/odnols/inventario-mine/main/IMG/Itens/new/${lista_itens[i].tipo_item}/${lista_itens[i].nome_icon}`;
 
+                    objeto_encontrado = true;
                     let nome_item = lista_itens[i].nome_item;
+                    let nome_pesquisa_wiki = nome_item;
+
                     let colet_suv = utilitarios[9]["sim"];
                     let empilhavel = `${utilitarios[9]["ate"]} ${lista_itens[i].empilhavel}`;
                     let renovavel = utilitarios[9]["sim"];
-
+                    
                     let tipo_item = lista_itens[i].tipo_item;
 
                     if (lista_itens[i].tipo_item === "Construcao")
@@ -90,7 +109,7 @@ module.exports = {
 
                                 valores_item = valores_item.replace("[&r", "");
                                 nome_item += ` | ${valores_item}`;
-
+                                nome_pesquisa_wiki = valores_item;
                             }
 
                             if (nome_item === "Livro encantado") {
@@ -98,6 +117,7 @@ module.exports = {
 
                                 valores_item = valores_item.replace("[&r", "");
                                 nome_item += ` | ${valores_item}`;
+                                nome_pesquisa_wiki = valores_item;
                             } else {
 
                                 valores_item = valores_item.replace("[&s[&3Efeito aplicado: ", "");
@@ -145,52 +165,90 @@ module.exports = {
                     
                     if (lista_itens[i].versao_add > 19)
                         nota_rodape = utilitarios[9]["nota_rodape"];
+                    
+                    if(objeto_encontrado){
+                        // Procurando na wiki sobre a pesquisa
+                        fetch(`https://minecraft.fandom.com/pt/wiki/${nome_pesquisa_wiki}`)
+                        .then(response => response.text())
+                        .then(async res => {
+                            
+                            let descricao_item_wiki;
 
-                    const embed = new MessageEmbed()
-                        .setTitle(nome_item)
-                        .setColor(0x29BB8E)
-                        .setImage(url)
-                        .addFields(
-                            {
-                                name: `${emoji_suv}** ${utilitarios[9]["coletavel"]}**`,
-                                value: `\`${colet_suv}\``,
-                                inline: true
-                            },
-                            {name: `:label: **${utilitarios[9]["tipo"]}**`, value: `\`${tipo_item}\``, inline: true},
-                            {
-                                name: `:bookmark_tabs: **${utilitarios[9]["versao_add"]}**`,
-                                value: `\`1.${lista_itens[i].versao_add}\``,
-                                inline: true
-                            },
-                        )
-                        .addFields(
-                            {
-                                name: `:abacus: **${utilitarios[9]["empilhavel"]}**`,
-                                value: `\`${empilhavel}\``,
-                                inline: true
-                            },
-                            {
-                                name: `:herb: **${utilitarios[9]["renovavel"]}**`,
-                                value: `\`${renovavel}\``,
-                                inline: true
-                            },
-                            {
-                                name: `:link: **${utilitarios[9]["nome_interno"]}**`,
-                                value: `**\`minecraft:${lista_itens[i].nome_interno}\`**`,
-                                inline: true
-                            }, fields
-                        )
-                        .setFooter(nota_rodape);
+                            try{ // Verifica se o item possui uma breve descrição
+                                descricao_item_wiki = res.split(`<meta name="description" content="`)[1];
+                                descricao_item_wiki = descricao_item_wiki.split(`"/>`)[0];
+                            }catch(err){
+                                descricao_item_wiki = "";
+                            }
 
-                    return message.reply({embeds: [embed]});
+                            const embed = new MessageEmbed()
+                            .setTitle(nome_item)
+                            .setColor(0x29BB8E)
+                            .setImage(url)
+                            .addFields(
+                                {
+                                    name: `${emoji_suv}** ${utilitarios[9]["coletavel"]}**`,
+                                    value: `\`${colet_suv}\``,
+                                    inline: true
+                                },
+                                {name: `:label: **${utilitarios[9]["tipo"]}**`, value: `\`${tipo_item}\``, inline: true},
+                                {
+                                    name: `:bookmark_tabs: **${utilitarios[9]["versao_add"]}**`,
+                                    value: `\`1.${lista_itens[i].versao_add}\``,
+                                    inline: true
+                                }
+                            )
+                            .addFields(
+                                {
+                                    name: `:abacus: **${utilitarios[9]["empilhavel"]}**`,
+                                    value: `\`${empilhavel}\``,
+                                    inline: true
+                                },
+                                {
+                                    name: `:herb: **${utilitarios[9]["renovavel"]}**`,
+                                    value: `\`${renovavel}\``,
+                                    inline: true
+                                },
+                                {
+                                    name: `:link: **${utilitarios[9]["nome_interno"]}**`,
+                                    value: `**\`minecraft:${lista_itens[i].nome_interno}\`**`,
+                                    inline: true
+                                }, fields
+                            )
+                            .setFooter(nota_rodape);
+
+                            if(descricao_item_wiki !== ""){
+                                
+                                let link_artigo = `https://minecraft.fandom.com/pt/wiki/${nome_pesquisa_wiki.replaceAll(" ", "_")}`;
+                                
+                                link_artigo = `${utilitarios[9]["mais_detalhes_wiki"].replace("link_repl", link_artigo)}`;
+
+                                embed.addFields(
+                                    {
+                                        name: `${logo_wikipedia} Wiki sobre ${nome_item}`,
+                                        value: `\`\`\`fix\n${descricao_item_wiki}\`\`\`\n${link_artigo}`,
+                                        inline: false
+                                    }
+                                )
+                            }
+
+                            return message.reply({embeds: [embed]});
+                        })
+                    }
                 }
+                
+                i++;
             }
+            
+            if(!objeto_encontrado){
+                const emoji_nao_encontrado = client.emojis.cache.get(emojis_negativos[Math.round((emojis_negativos.length - 1) * Math.random())]).toString();
 
-            const emoji_nao_encontrado = client.emojis.cache.get(emojis_negativos[Math.round((emojis_negativos.length - 1) * Math.random())]).toString();
-
-            message.reply(`${emoji_nao_encontrado} | ${utilitarios[9]["nao_encontrado"]} \`${pesquisa}\`, ${utilitarios[9]["tente_novamente"]}`);
+                message.reply(`${emoji_nao_encontrado} | ${utilitarios[9]["nao_encontrado"]} \`${pesquisa}\`, ${utilitarios[9]["tente_novamente"]}`);
+            }
         })
-        .catch(() => {
+        .catch(err => {
+            console.log(err);
+
             return message.reply(`:octagonal_sign: | ${utilitarios[9]["error_1"]}`);
         });
     }
