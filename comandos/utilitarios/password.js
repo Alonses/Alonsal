31b1355df7231d3fe48c459e22cefc75d7cab358
Gustavo <gusbra21@outlook.com>
2022-07-28 22:08:08 -0300

    Correções no psw

--- comandos/utilitarios/password.js
+++ comandos/utilitarios/password.js
@@ -41,13 +41,18 @@ module.exports = {
 }
 
 function randomString(len) {
-    const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%¨&*()^[]{}+=~.,;:¢¬_-£"|\\/?§';
+    const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdeeͪfghijklmnopqrstuvwxyz0123456789!@#$%¨&*()^[]{}+=~.,;:¢¬_-£"|\\/?§';
     let randomString = '';
 
     for (let i = 0; i < len; i++) {
         let randomPoz = Math.floor(Math.random() * charSet.length);
-        randomString += charSet.substr(randomPoz, randomPoz + 1);
+        randomString += charSet.slice(randomPoz, randomPoz + 1);
     }
 
-    return randomString;
+    return shuffle(randomString.split(''))
 }
+
+function shuffle(o) {
+    for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o.join("");
+  }
