cd6ec7a2c3300166d96706c471ff1ab1fdd551cc
Gustavo <gusbra21@outlook.com>
2022-08-27 12:01:20 -0300

    ajustes

--- adm/funcoes/formata_horas.js
+++ adm/funcoes/formata_horas.js
@@ -1,9 +1,9 @@
 module.exports = (horas, minutos, segundos) => {
 
-    if(minutos)
-        return `${(`0${horas}`).substr(-2)}:${(`0${minutos}`).substr(-2)}`
-    else if(segundos)
+    if(segundos)
         return `${(`0${horas}`).substr(-2)}:${(`0${minutos}`).substr(-2)}:${(`0${segundos}`).substr(-2)}`
+    else if(minutos)
+        return `${(`0${horas}`).substr(-2)}:${(`0${minutos}`).substr(-2)}`
     else
         return (`0${horas}`).substr(-2)
 }
