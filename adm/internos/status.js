849e404868965a427d26cc0ab1e86cd232f5e5aa
Gustavo <gusbra21@outlook.com>
2022-08-30 23:58:15 -0300

    Correções

--- adm/internos/status.js
+++ adm/internos/status.js
@@ -6,15 +6,7 @@ const { EmbedBuilder } = require('discord.js')
 
 module.exports = async ({client}) => {
 
-    if (client.user.id === "833349943539531806"){
-
-        const canais_texto = client.channels.cache.filter((c) => c.type === 0).size
-        const canais_voz = client.channels.cache.filter((c) => c.type === 2).size
-        let members = 0
-        
-        client.guilds.cache.forEach(async guild => {
-            members += guild.memberCount - 1
-        })
+    // if (client.user.id === "833349943539531806"){
 
         fetch('https://apisal.herokuapp.com/status')
         .then(res => res.json())
@@ -24,30 +16,10 @@ module.exports = async ({client}) => {
             if(retorno.status)
                 status_apisal = "✅ Online"
 
-            const embed = new EmbedBuilder()
-            .setTitle(':steam_locomotive: Caldeiras aquecidas')
-            .setColor(0x29BB8E)
-            .addFields(
-                {
-                    name: ':globe_with_meridians: **Servidores**',
-                    value: `**Ativo em: **\`${client.guilds.cache.size}\``,
-                    inline: true
-                },
-                {
-                    name: ':card_box: **Canais**',
-                    value: `**Observando: **\`${canais_texto}\`\n**Falando em: ** \`${canais_voz}\``,
-                    inline: true
-                },
-                {
-                    name: ':busts_in_silhouette: **Usuários**',
-                    value: `**Escutando: **\`${members}\``,
-                    inline: true
-                }
-            )
-            .addFields({name: ':white_small_square: Versão', value: `\`${version}\`\n**APISAL: **${status_apisal}`, inline: false })
-            .setFooter({ text: client.user.username, iconURL: client.user.avatarURL({ dynamic: true })})
-            
-            client.channels.cache.get('854695578372800552').send({ embeds: [embed] }) // Avisa que está online em um canal
+            dispara_status(client, status_apisal)
+        })
+        .catch(() => {
+            dispara_status(client, "🛑 Offline")
         })
         
         client.user.setActivity('Faites chauffer la vapeur!', 'COMPETING')
@@ -63,9 +35,48 @@ module.exports = async ({client}) => {
 
         let i = 0
         setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`), 10000)
-    } else
-        client.user.setPresence({ activities: [{ name: 'baidu nos servidores' }] })
+    // } else
+        // client.user.setPresence({ activities: [{ name: 'baidu nos servidores' }] })
 
     require('./relata.js')({client})
     require('./anuncia.js')({client})
+}
+
+function dispara_status(client, status_apisal){
+
+    const canais_texto = client.channels.cache.filter((c) => c.type === 0).size
+    const canais_voz = client.channels.cache.filter((c) => c.type === 2).size
+    let members = 0
+    
+    client.guilds.cache.forEach(async guild => {
+        members += guild.memberCount - 1
+    })
+
+    const embed = new EmbedBuilder()
+    .setTitle(':steam_locomotive: Caldeiras aquecidas')
+    .setColor(0x29BB8E)
+    .addFields(
+        {
+            name: ':globe_with_meridians: **Servidores**',
+            value: `**Ativo em: **\`${client.guilds.cache.size}\``,
+            inline: true
+        },
+        {
+            name: ':card_box: **Canais**',
+            value: `**Observando: **\`${canais_texto}\`\n**Falando em: ** \`${canais_voz}\``,
+            inline: true
+        },
+        {
+            name: ':busts_in_silhouette: **Usuários**',
+            value: `**Escutando: **\`${members}\``,
+            inline: true
+        }
+    )
+    .addFields(
+        {name: ':white_small_square: Versão', value: `\`${version}\``, inline: true },
+        {name: '⠀', value: '⠀', inline: true},
+        {name: ':moyai: APISAL', value: `\`${status_apisal}\``, inline: true})
+    .setFooter({ text: client.user.username, iconURL: client.user.avatarURL({ dynamic: true })})
+    
+    client.channels.cache.get('854695578372800552').send({ embeds: [embed] }) // Avisa que está online em um canal
 }
