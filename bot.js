c12f26ca5b49a5ed02a64b9d12df82e08285c2af
Gustavo <gustavobrandao31@outlook.com>
2022-09-03 15:11:45 -0300

    🕵️

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ const client = new Client({
 })
 
 // Alternância entre modo normal e de testes
-const modo_develop = 1, force_update = 0
+const modo_develop = 0, force_update = 0
 let status = 1, ranking = 1
 
 if(!modo_develop){
