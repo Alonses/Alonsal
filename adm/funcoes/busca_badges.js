73218d87787cf21192da78f0dc6cd1969054e656
Gustavo <gustavobrandao31@outlook.com>
2022-09-07 19:47:45 -0300

    Badges 1.0

---
+++ adm/funcoes/busca_badges.js
@@ -0,0 +1,37 @@
+const { existsSync } = require('fs')
+
+const { emojis } = require('../../arquivos/json/text/emojis.json')
+const busca_emoji = require('./busca_emoji.js')
+
+module.exports = (client, modo, id_usuario) => {
+
+    const user = {
+        id: id_usuario,
+        fixed_badge: null,
+        badge_list: []
+    }
+
+    let all_badges = []
+    const badges = [emojis.aln_tester, emojis.aln_debugger, emojis.aln_programmer, emojis.aln_creator]
+    const badge_names = ["Tester", "Debugger", "Programmer", "Creator"]
+
+    if (existsSync(`./arquivos/data/badges/${user.id}/badges.json`)) {
+        delete require.cache[require.resolve(`../../arquivos/data/badges/${user.id}/badges.json`)]
+        const { fixed_badge, badge_list } = require(`../../arquivos/data/badges/${user.id}/badges.json`)
+
+        user.fixed_badge = fixed_badge
+        
+        badge_list.forEach(valor => {
+            let emoji_badge = busca_emoji(client, badges[parseInt(Object.keys(valor)[0])])
+
+            // Listando todas as badges que o usuário possui
+            if (modo == "fixed") {
+                if (fixed_badge == parseInt(Object.keys(valor)[0]))
+                    all_badges.push(`${emoji_badge} \`${badge_names[parseInt(Object.keys(valor)[0])]}\`, ganhou em <t:${Object.values(valor)[0]}:f>`)
+            } else
+                all_badges.push(`${emoji_badge} \`${badge_names[parseInt(Object.keys(valor)[0])]}\`, ganhou em <t:${Object.values(valor)[0]}:f>`) // Listando todas as badges que o usuário possui
+        })
+    }
+
+    return all_badges.join("\n")
+}
---
+++ comandos/config/conf_badges.js
@@ -0,0 +1,86 @@
+const { SlashCommandBuilder } = require('discord.js')
+const { existsSync, mkdirSync, writeFileSync } = require('fs')
+
+const { emojis_dancantes, emojis } = require('../../arquivos/json/text/emojis.json')
+const busca_emoji = require('../../adm/funcoes/busca_emoji.js')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('c_badge')
+		.setDescription('⌠✳️⌡ Atribuir badges a usuários')
+        .addStringOption(option =>
+            option.setName('id')
+            .setDescription("O ID do usuário alvo")
+            .setRequired(true))
+        .addStringOption(option =>
+            option.setName('badge')
+                .setDescription('A badge que será atribuida')
+                .addChoices(
+                    { name: 'Tester', value: '0' },
+                    { name: 'Debugger', value: '1' },
+                    { name: 'Programmer', value: '2' },
+                    { name: 'Creator', value: '3' }
+                )
+                .setRequired(true)),
+    async execute(client, interaction) {
+
+        if (!client.owners.includes(interaction.user.id)) return
+
+        const user = {
+            id: null,
+            badge: null,
+            fixed_badge: null,
+            badge_list: []
+        }
+
+        const badges = [emojis.aln_tester, emojis.aln_debugger, emojis.aln_programmer, emojis.aln_creator]
+        const badge_names = ["Tester", "Debugger", "Programmer", "Creator"]
+        const all_badges = []
+
+        let entradas = interaction.options.data
+        const emoji_dancante = busca_emoji(client, emojis_dancantes)
+
+        entradas.forEach(valor => {
+            if (valor.name == "id")
+                user.id = valor.value
+
+            if (valor.name == "badge")   
+                user.badge = parseInt(valor.value)
+        })
+        
+        const badge = busca_emoji(client, badges[user.badge])
+    
+        // Criando o JSON para o usuário da badge
+        if (!existsSync(`./arquivos/data/badges/${user.id}`))
+            mkdirSync(`./arquivos/data/badges/${user.id}`, { recursive: true })
+
+        if (existsSync(`./arquivos/data/badges/${user.id}/badges.json`)) {
+            delete require.cache[require.resolve(`../../arquivos/data/badges/${user.id}/badges.json`)]
+            const { fixed_badge, badge_list } = require(`../../arquivos/data/badges/${user.id}/badges.json`)
+
+            user.fixed_badge = fixed_badge
+            
+            badge_list.forEach(valor => {
+                user.badge_list.push(valor)
+                all_badges.push(parseInt(Object.keys(valor)[0])) // Listando todas as badges que o usuário possui
+            })
+        }
+
+        const date1 = new Date()
+        if (!all_badges.includes(user.badge)) // Adicionando uma nova badge
+            user.badge_list.push(constructJson(user.badge, Math.floor(date1.getTime() / 1000)))
+
+        writeFileSync(`./arquivos/data/badges/${user.id}/badges.json`, JSON.stringify(user))
+        delete require.cache[require.resolve(`../../arquivos/data/badges/${user.id}/badges.json`)]
+
+        client.users.fetch(user.id, false).then((user_interno) => {
+            user_interno.send(`${emoji_dancante} | Você acabou de ganhar uma Badge! O \`${badge_names[user.badge]}\` ${badge}! Ele será exibido em seu perfil ao usarem o comando \`/user info\`\n\nVocê também pode fixar Badges em destaque com o comando \`/badges\`!`)
+            
+            interaction.reply({ content: `${emoji_dancante} | Badge \`${badge_names[user.badge]}\` ${badge} atribuída ao usuário ${user_interno}!`, ephemeral: true })
+        })
+    }
+}
+
+function constructJson(jsonGuild, arrayValores) {
+	return { [jsonGuild] : arrayValores } 
+}
