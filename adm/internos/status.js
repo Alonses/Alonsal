87fe8afbcce90c693fe0ee448ecdfa9e8860519f
Gustavo <gustavobrandao31@outlook.com>
2022-09-09 17:25:40 -0300

    Webhooks

--- adm/internos/status.js
+++ adm/internos/status.js
@@ -7,8 +7,6 @@ const { EmbedBuilder } = require('discord.js')
 module.exports = async ({client}) => {
     
     if (client.user.id === "833349943539531806") {
-
-        // require('./commit.js')({client})
         
         fetch('https://apisal.herokuapp.com/status')
         .then(res => res.json())
