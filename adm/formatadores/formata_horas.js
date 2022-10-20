e4049b001f77f89c254b10f39289d809c0d30446
Gustavo <gustavobrandao31@outlook.com>
2022-10-20 00:14:22 -0300

    Correção de bugs

--- adm/formatadores/formata_horas.js
+++
@@ -1,9 +0,0 @@
-module.exports = (horas, minutos, segundos) => {
-
-    if (segundos)
-        return `${(`0${horas}`).substr(-2)}:${(`0${minutos}`).substr(-2)}:${(`0${segundos}`).substr(-2)}`
-    else if (minutos)
-        return `${(`0${horas}`).substr(-2)}:${(`0${minutos}`).substr(-2)}`
-    else
-        return (`0${horas}`).substr(-2)
-}
