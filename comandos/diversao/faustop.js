b0bb3d3fc7175ee05b0f31746e43195ecdd1f4a6
odnols <gustavobrandao31@outlook.com>
2023-01-22 00:28:35 -0300

    Novos sons e melhorias

--- comandos/diversao/faustop.js
+++ comandos/diversao/faustop.js
@@ -16,7 +16,7 @@ module.exports = {
 
         let i = 0
 
-        for (const file of readdirSync(`./arquivos/songs/faustop`).filter(file => file.endsWith('.mp3')))
+        for (const file of readdirSync(`./arquivos/songs/faustop`).filter(file => file.endsWith('.ogg')))
             i++
 
         const data = new Date()
@@ -25,7 +25,7 @@ module.exports = {
         if (data.getHours() == 20 && data.getMinutes() == 7)
             num = Math.round(1 + (1 * Math.random())) > 1 ? 7 : 12;
 
-        const file = new AttachmentBuilder(`./arquivos/songs/faustop/faustop_${num}.mp3`, { name: 'faustop.mp3' })
+        const file = new AttachmentBuilder(`./arquivos/songs/faustop/faustop_${num}.ogg`, { name: 'faustop.ogg' })
 
         return interaction.reply({ files: [file] })
     }
