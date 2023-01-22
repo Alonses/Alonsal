febc6eb2470c4a3a5c3bf60914182bafbaa6bc2a
odnols <gustavobrandao31@outlook.com>
2023-01-21 22:38:33 -0300

    /calc e /gatos

---
+++ comandos/diversao/gatos.js
@@ -0,0 +1,30 @@
+const fetch = (...args) =>
+    import('node-fetch').then(({ default: fetch }) => fetch(...args))
+
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('cats')
+        .setNameLocalizations({
+            "pt-BR": "gatos",
+            "es-ES": 'gatos',
+            "fr": 'chats',
+            "it": 'gatti'
+        })
+        .setDescription('⌠🐱⌡ Gatos!')
+        .setDescriptionLocalizations({
+            "pt-BR": "⌠🐱⌡ Gatos!",
+            "es-ES": '⌠🐱⌡ Gatos!',
+            "fr": '⌠🐱⌡ Chats!',
+            "it": '⌠🐱⌡ Gatti!'
+        }),
+    async execute(client, interaction) {
+
+        fetch(`https://api.thecatapi.com/v1/images/search?api_key=${process.env.catapi}`)
+            .then(res => res.json())
+            .then(res => {
+                interaction.reply(res[0].url)
+            })
+    }
+}
