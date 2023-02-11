9f92c1d8927bab5826935c198d453132caf489a7
odnols <gusbra21@outlook.com>
2023-02-11 18:32:11 -0300

    GTAWeather

--- comandos/jogos/gta.js
+++ comandos/jogos/gta.js
@@ -1,66 +1,48 @@
-const { SlashCommandBuilder } = require('discord.js')
+const fetch = (...args) =>
+    import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
-const GTADAY = 48 // minutos
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("gta")
-        .setDescription("⌠🎲⌡ Shows the current time in GTA Online")
+        .setDescription("⌠🎲⌡ GTA Online information")
         .setDescriptionLocalizations({
-            "pt-BR": '⌠🎲⌡ Mostra a hora atual no GTA Online',
-            "es-ES": '⌠🎲⌡ Muestra la hora actual en GTA Online',
-            "fr": '⌠🎲⌡ Affiche l\'heure actuelle dans GTA Online',
-            "it": '⌠🎲⌡ Mostra l\'ora corrente in GTA Online',
-            "ru": '⌠🎲⌡ Показывает текущее время в GTA Online'
+            "pt-BR": '⌠🎲⌡ Informações do GTA Online',
+            "es-ES": '⌠🎲⌡ Información de GTA Online',
+            "fr": '⌠🎲⌡ Informations sur GTA Online',
+            "it": '⌠🎲⌡ Informazioni su GTA Online',
+            "ru": '⌠🎲⌡ ГТА Онлайн информация'
         }),
     async execute(client, user, interaction) {
 
-        let currentDate = new Date()
+        fetch(`${process.env.url_apisal}/gta`)
+            .then(response => response.json())
+            .then(async data => {
 
-        // Previne erros entre meia noite e 5 da manhã
-        let hora_variavel = currentDate.getHours() - 5 > 0 ? currentDate.getHours() - 5 : 19 + currentDate.getHours()
-        let horaAtual = `${hora_variavel}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`
+                // let emoji_horario = data.gameTimeStr.slice(0, 2) < 20 && data.gameTimeStr.slice(0, 2) > 6 ? data.gameTimeStr.slice(0, 2) > 17 ? ":city_sunset:" : ":park:" : ":bridge_at_night:"
 
-        const hora_gta = dateGTA(timeToMin(horaAtual))
+                let hours = data.gameTimeStr.split(":")[0] % 12
+                hours = hours ? hours : 12
 
-        let emoji_horario = hora_gta.slice(0, 2) < 20 && hora_gta.slice(0, 2) > 6 ? hora_gta.slice(0, 2) > 17 ? ":city_sunset:" : ":park:" : ":bridge_at_night:"
+                if (data.gameTimeStr.split(":")[1] >= 30)
+                    hours += "30"
 
-        interaction.reply({ content: `${emoji_horario} | ${client.tls.phrase(user, "game.gta.horario").replace("horario_repl", hora_gta)}`, ephemeral: user.misc.ghost_mode })
+                const embed = new EmbedBuilder()
+                    .setTitle("> This is Weazel News")
+                    .setColor(client.embed_color(user.misc.color))
+                    .setDescription(`\`\`\`${data.currentWeatherEmoji} - ${data.currentWeatherDescription}\`\`\``)
+                    .addFields(
+                        {
+                            name: `Horário atual`,
+                            value: `:clock${hours}: \`${data.gameTimeStr}\``,
+                            inline: true
+                        }
+                    )
+                interaction.reply({ embeds: [embed], ephemeral: user.misc.ghost_mode })
+            })
+            .catch(err => {
+                interaction.reply({ content: ':octagonal_sign: | Não foi possível conectar a APISAL no momento, por favor, tente novamente mais tarde!', ephemeral: true })
+            })
     }
-}
-
-function timeToMin(date = "") {
-
-    /*
-       Transforma a data atual em minutos
-    */
-
-    const times = date.split(":").map(val => parseInt(val))
-    return (times[0] * 60) + times[1] + (times[2] / 60)
-}
-
-const decHourToMin = (time) => {
-
-    /*
-      Tranforma o numero decimal das horas em minutos 
-      Ex. "12.5" seria "12:30"
-    */
-
-    const hour = Math.floor(time)
-    const min = time - hour
-
-    return min * 60
-}
-
-function dateGTA(time) {
-
-    const min = time % 48
-
-    const hour = Math.floor((min / GTADAY) * 24)
-    const minuts = Math.floor(decHourToMin((min / GTADAY) * 24))
-
-    const hora = hour < 10 ? `0${hour}` : hour
-    const minutos = minuts < 10 ? `0${minuts}` : minuts
-
-    return `${hora}:${minutos}`
 }
