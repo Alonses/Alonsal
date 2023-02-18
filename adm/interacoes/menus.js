4a78063e3d8f8cd431238f286f459ec6d27a712d
odnols <gustavobrandao31@outlook.com>
2023-02-18 11:31:07 -0300

    Novas interações com menu e correções

--- adm/interacoes/menus.js
+++ adm/interacoes/menus.js
@@ -1,14 +1,24 @@
+const { AttachmentBuilder } = require('discord.js')
+
 const { busca_badges, badgeTypes } = require('../../adm/data/badges')
 
 module.exports = async ({ client, user, interaction }) => {
 
-    if (interaction.customId === `select_${interaction.user.id}`) {
+    if (interaction.customId === `select_badges_${interaction.user.id}`) {
 
+        // Fixando a badge escolhida pelo usuário
         user.misc.fixed_badge = interaction.values[0]
 
         user.save()
         let new_badge = busca_badges(client, badgeTypes.SINGLE, parseInt(interaction.values[0]))
 
         interaction.update({ content: `${new_badge.emoji} | Badge \`${new_badge.name}\` ${client.tls.phrase(user, "dive.badges.badge_fixada")}`, components: [], ephemeral: true })
+
+    } else if (interaction.customId === `select_fausto_${interaction.user.id}`) {
+
+        // Enviando uma das frases do faustão selecionada pelo menu
+        const file = new AttachmentBuilder(`./arquivos/songs/faustop/faustop_${interaction.values[0]}.ogg`, { name: 'faustop.ogg' })
+
+        interaction.update({ content: "", files: [file], components: [], ephemeral: user?.conf.ghost_mode || false })
     }
 }
--- comandos/diversao/faustop.js
+++ comandos/diversao/faustop.js
@@ -1,6 +1,9 @@
 const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js')
 
 const { readdirSync } = require('fs')
+const create_menus = require('../../adm/discord/create_menus.js')
+
+const { relation } = require('../../arquivos/songs/faustop/songs.json')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,22 +15,35 @@ module.exports = {
             "fr": '⌠😂⌡ Les phrases de Faustão',
             "it": '⌠😂⌡ Le frasi di Faustão',
             "ru": '⌠😂⌡ Фразы от Faustão'
-        }),
+        })
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("rand")
+                .setDescription("⌠😂|🇧🇷⌡ Invoca uma frase aleatória do faustão"))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("menu")
+                .setDescription("⌠😂|🇧🇷⌡ Escolher uma frase do faustão")),
     async execute(client, user, interaction) {
 
         let i = 0
 
-        for (const file of readdirSync(`./arquivos/songs/faustop`).filter(file => file.endsWith('.ogg')))
-            i++
+        if (interaction.options.getSubcommand() !== "menu") {
+            for (const file of readdirSync(`./arquivos/songs/faustop`).filter(file => file.endsWith('.ogg')))
+                i++
 
-        const data = new Date()
-        let num = Math.round((i - 1) * Math.random())
+            const data = new Date()
+            let num = Math.round((i - 1) * Math.random())
 
-        if (data.getHours() == 20 && data.getMinutes() == 7)
-            num = Math.round(1 + (1 * Math.random())) > 1 ? 7 : 12;
+            if (data.getHours() == 20 && data.getMinutes() == 7)
+                num = Math.round(1 + (1 * Math.random())) > 1 ? 7 : 12;
 
-        const file = new AttachmentBuilder(`./arquivos/songs/faustop/faustop_${num}.ogg`, { name: 'faustop.ogg' })
+            const file = new AttachmentBuilder(`./arquivos/songs/faustop/faustop_${num}.ogg`, { name: 'faustop.ogg' })
 
-        return interaction.reply({ files: [file], ephemeral: user?.conf.ghost_mode || false })
+            return interaction.reply({ files: [file], ephemeral: user?.conf.ghost_mode || false })
+        } else {
+
+            return interaction.reply({ content: 'Escolha uma das frases abaixo!', components: [create_menus("fausto", client, interaction, user, relation)], ephemeral: user?.conf.ghost_mode })
+        }
     }
 }
