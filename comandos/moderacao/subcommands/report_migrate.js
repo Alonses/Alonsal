94551828346ec442f53eebaa84ee3c1d3cbcf606
odnols <gustavobrandao31@outlook.com>
2023-05-28 18:05:08 -0300

    Fragmentando subcomandos 3.0

---
+++ comandos/moderacao/subcommands/report_migrate.js
@@ -0,0 +1,16 @@
+const { EmbedBuilder } = require('discord.js')
+
+module.exports = async ({ client, user, interaction }) => {
+
+    // Enviando o embed para validação
+    const embed = new EmbedBuilder()
+        .setTitle(client.tls.phrase(user, "mode.report.automatizado"))
+        .setColor(0xED4245)
+        .setDescription(client.tls.phrase(user, "mode.report.descricao_automatizado"))
+        .setFooter({ text: client.tls.phrase(user, "menu.botoes.selecionar_operacao"), iconURL: client.discord.user.avatarURL({ dynamic: true }) })
+
+    // Criando os botões para a cor customizada
+    const row = client.create_buttons([{ id: "report_auto", name: client.tls.phrase(user, "menu.botoes.confirmar"), value: '1', type: 2, emoji: client.emoji(10), data: 1 }, { id: "report_auto", name: client.tls.phrase(user, "menu.botoes.cancelar"), value: '0', type: 3, emoji: client.emoji(0), data: 0 }], interaction)
+
+    return interaction.reply({ embeds: [embed], components: [row], ephemeral: true })
+}
---
+++ comandos/moderacao/subcommands/report_remove.js
@@ -0,0 +1,10 @@
+module.exports = async ({ client, user, interaction, alvo }) => {
+
+    // Retirando o reporte do usuário e anexando uma justificativa
+    alvo.archived = true
+    alvo.relatory = `${alvo.relatory}\n🔰 | ${interaction.options.getString("reason")}`
+
+    client.tls.reply(interaction, user, "mode.report.usuario_att", true, 4)
+
+    await alvo.save()
+}
