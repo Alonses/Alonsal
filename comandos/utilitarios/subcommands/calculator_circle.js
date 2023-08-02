07cf3d8e5984d592cd985c780c985200ebcc192c
odnols <gusbra21@outlook.com>
2023-08-02 20:55:04 -0300

    Indentando/ ajustando

--- comandos/utilitarios/subcommands/calculator_circle.js
+++ comandos/utilitarios/subcommands/calculator_circle.js
@@ -9,7 +9,7 @@ module.exports = async ({ client, user, interaction }) => {
 
     if (operacao === "1") { // Diâmetro
 
-        descricao = `html\n${client.defaultEmoji("metrics")} Etapas\n\nEntrada, 🚫 Diâmetro = \`${client.locale(raio)}\`\nÉ necessário ajustar a fórmula do diâmetro para descobrir o raio.\n\nFórmula do diâmetro: d = r * 2\nFórmula ajustada: r = d / 2 (isola-se o raio)\nNa conta: r = ${client.locale(raio)} / 2`
+        descricao = `html\n${client.defaultEmoji("metrics")} Etapas\n\nEntrada, 🚫 Diâmetro = ${client.locale(raio)}\nÉ necessário ajustar a fórmula do diâmetro para descobrir o raio.\n\nFórmula do diâmetro: d = r * 2\nFórmula ajustada: r = d / 2 (isola-se o raio)\nNa conta: r = ${client.locale(raio)} / 2`
         raio = raio / 2
 
         descricao += `\n↳ r = ${client.locale(raio)}`
@@ -17,7 +17,7 @@ module.exports = async ({ client, user, interaction }) => {
 
     if (operacao === "2") { // Perímetro
 
-        descricao = `html\n${client.defaultEmoji("metrics")} Etapas\n\nEntrada, ⭕ Perímetro = \`${client.locale(raio)}\`\nÉ necessário ajustar a fórmula do perímetro para descobrir o raio.\n\nFórmula do perímetro: p = r * π * 2\nFórmula ajustada: r = p / (2 * π) (isola-se o raio)\nNa conta: r = ${client.locale(raio)} / (2 * π)`
+        descricao = `html\n${client.defaultEmoji("metrics")} Etapas\n\nEntrada, ⭕ Perímetro = ${client.locale(raio)}\nÉ necessário ajustar a fórmula do perímetro para descobrir o raio.\n\nFórmula do perímetro: p = r * π * 2\nFórmula ajustada: r = p / (2 * π) (isola-se o raio)\nNa conta: r = ${client.locale(raio)} / (2 * π)`
         raio = raio / (2 * math.pi)
 
         descricao += `\n↳ r = ${client.locale(raio)}`
@@ -25,7 +25,7 @@ module.exports = async ({ client, user, interaction }) => {
 
     if (operacao === "3") { // Área
 
-        descricao = `html\n${client.defaultEmoji("metrics")} Etapas\n\nEntrada, ⚪ Área = \`${client.locale(raio)}\`\nÉ necessário ajustar a fórmula da área para descobrir o raio.\n\nFórmula da área: a = π * r²\nFórmula ajustada: r² = a / π (isola-se o raio)\nNa conta: r² = ${client.locale(raio)} / π`
+        descricao = `html\n${client.defaultEmoji("metrics")} Etapas\n\nEntrada, ⚪ Área = ${client.locale(raio)}\nÉ necessário ajustar a fórmula da área para descobrir o raio.\n\nFórmula da área: a = π * r²\nFórmula ajustada: r² = a / π (isola-se o raio)\nNa conta: r² = ${client.locale(raio)} / π`
         raio = raio / math.pi
 
         descricao += `\n↳ r² = ${client.locale(raio)}\n\nApós, retiramos a raiz quadrada desse valor.\nSendo: r = √${client.locale(raio)}`
