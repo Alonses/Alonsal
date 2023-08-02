fe1d0287eec48f6cc7b1850afad7991f72ebb59c
lllggghhhaaa <lgha1423@outlook.com>
2023-08-02 20:08:22 -0300

    clear context menu command

--- comandos/moderacao/clear.js
+++ comandos/moderacao/clear.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits, PermissionsBitField } = require('discord.js')
+const { ContextMenuCommandBuilder, ApplicationCommandType, SlashCommandBuilder, PermissionFlagsBits, PermissionsBitField } = require('discord.js')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -32,24 +32,42 @@ module.exports = {
                 .setMaxValue(100)
                 .setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+    menu_data: new ContextMenuCommandBuilder()
+	.setName("Clear")
+	.setType(ApplicationCommandType.Message)
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
     async execute(client, user, interaction) {
-
-        const membro_sv = await client.getUserGuild(interaction, client.id())
         const qtd_msg = interaction.options.getInteger("amount")
+        deleteMessages(client, user, interaction, qtd_msg)
+    },
+    async menu(client, user, interaction) {
+        const messageDate = interaction.targetMessage.createdAt;
 
-        // Verificando se o bot pode gerenciar as mensagens do servidor
-        if (!membro_sv.permissions.has(PermissionsBitField.Flags.ManageMessages))
-            return client.tls.reply(interaction, user, "mode.clear.permissao", true, 3)
+        interaction.targetMessage.channel.messages.fetch()
+        .then(messages => {
+            const count = messages.filter(m => m.createdAt >= messageDate).size;
 
-        const channel = interaction.channel
-
-        channel.bulkDelete(qtd_msg, true)
-            .then(messages => {
-                const count = messages.size;
-                const texto = count > 1 ? `\`${count} ${client.tls.phrase(user, "mode.clear.apagado_1")}` : `\`1 ${client.tls.phrase(user, "mode.clear.apagado_2")}`
-
-                interaction.reply({ content: `:recycle: | ${texto}`, ephemeral: true })
-            })
-            .catch(() => client.tls.reply(interaction, user, "mode.clear.error", true, 0))
+            deleteMessages(client, user, interaction, count)
+        })
+        .catch(console.error);
     }
+}
+
+async function deleteMessages(client, user, interaction, qtd_msg) {
+    const membro_sv = await client.getUserGuild(interaction, client.id())
+
+    // Verificando se o bot pode gerenciar as mensagens do servidor
+    if (!membro_sv.permissions.has(PermissionsBitField.Flags.ManageMessages))
+        return client.tls.reply(interaction, user, "mode.clear.permissao", true, 3)
+
+    const channel = interaction.channel
+
+    channel.bulkDelete(qtd_msg, true)
+        .then(messages => {
+            const count = messages.size;
+            const texto = count > 1 ? `\`${count} ${client.tls.phrase(user, "mode.clear.apagado_1")}` : `\`1 ${client.tls.phrase(user, "mode.clear.apagado_2")}`
+
+            interaction.reply({ content: `:recycle: | ${texto}`, ephemeral: true })
+        })
+        .catch(() => client.tls.reply(interaction, user, "mode.clear.error", true, 0))
 }
