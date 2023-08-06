399e652ab016dfd5766b03d2ea33791ed25f32d3
odnols <gusbra21@outlook.com>
2023-08-06 16:40:17 -0300

    Bank statements

---
+++ adm/database/schemas/Statement.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+
+// uid -> User ID
+
+const schema = new mongoose.Schema({
+    uid: { type: Number, default: 0 },
+    operation: { type: String, default: null },
+    type: { type: Boolean, default: false }, // false -> saída, true -> entrada
+    value: { type: Number, default: 0 },
+    timestamp: { type: Number, default: null }
+})
+
+const model = mongoose.model("Statement", schema)
+
+async function createStatement(uid, operation, type, value, timestamp) {
+
+    const statements = await getUserStatements(uid)
+    if (statements.length > 9) //  Exclui a última movimentação após 10 novas entradas
+        dropUserStatement(uid, statements[9].timestamp)
+
+    await model.create({ uid: uid, operation: operation, type: type, value: value, timestamp: timestamp })
+}
+
+async function getUserStatements(uid) {
+    return model.find({ uid: uid }).sort({ timestamp: -1 })
+}
+
+async function dropUserStatement(uid, timestamp) {
+    await model.findOneAndDelete({ uid: uid, timestamp: timestamp })
+}
+
+async function dropAllUserStatements(uid) {
+    await model.deleteMany({ uid: uid })
+}
+
+module.exports.Statement = model
+module.exports = {
+    createStatement,
+    getUserStatements,
+    dropUserStatement,
+    dropAllUserStatements
+}
