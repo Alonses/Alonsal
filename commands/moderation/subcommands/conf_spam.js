b312fd3e203001a10fd7bbc2ba4d593122da9644
odnols <gusbra21@outlook.com>
2023-09-16 20:24:06 -0300

    Permissões de canais para o anti-spam

--- commands/moderation/subcommands/conf_spam.js
+++ commands/moderation/subcommands/conf_spam.js
@@ -1,3 +1,5 @@
+const { PermissionsBitField } = require('discord.js')
+
 module.exports = async ({ client, user, interaction, guild }) => {
 
     let canal_alvo
@@ -10,13 +12,17 @@ module.exports = async ({ client, user, interaction, guild }) => {
             return client.tls.reply(interaction, user, "mode.report.tipo_canal", true, client.defaultEmoji("types"))
 
         // Atribuindo o canal passado para o antispam
-        canal_alvo = interaction.options.getChannel("channel").id
-        guild.logger.channel = canal_alvo
+        canal_alvo = interaction.options.getChannel("channel")
+        guild.logger.channel = canal_alvo.id
     }
 
     // Sem canal informado no comando e nenhum canal salvo no banco do bot
     if (!canal_alvo && !guild.logger.channel)
         return client.tls.reply(interaction, user, "mode.logger.mencao_canal", true, 1)
+
+    // Sem permissão para ver ou escrever no canal mencionado
+    if (!canal_alvo.permissionsFor(client.user()).has(PermissionsBitField.Flags.ViewChannel) || !canal_alvo.permissionsFor(client.user()).has(PermissionsBitField.Flags.SendMessages))
+        return client.tls.reply(interaction, user, "mode.logger.falta_escrita_visualizacao", true, 1)
 
     // Ativa ou desativa o sistema antispam no servidor
     if (!guild.conf.spam)
