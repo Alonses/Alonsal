3bf9e92bd9eafbb58b29e8dcab6c9929e24785d4
odnols <gustavobrandao31@outlook.com>
2023-09-20 18:48:08 -0300

    Ajustes com status customizados

--- core/events/presence.js
+++ core/events/presence.js
@@ -25,7 +25,7 @@ module.exports = async ({ client }) => {
         client.user().setActivity("Baidu explosivo", { type: ActivityType.Playing })
 }
 
-function requisita_status(client) {
+async function requisita_status(client) {
 
     let num
 
@@ -46,34 +46,33 @@ function requisita_status(client) {
     if (activities[num].type === 1)
         tempo_minimo = client.random(50000, 80000)
 
-    // Exibirá o status escolhido após um tempo aleatório
-    timeout_presence = setTimeout(async () => {
+    let texto_status = activities[num].text
 
-        let texto_status = activities[num].text
+    if (texto_status.includes("server_repl"))
+        texto_status = texto_status.replace("server_repl", client.guilds().size)
 
-        if (texto_status.includes("server_repl"))
-            texto_status = texto_status.replace("server_repl", client.guilds().size)
+    if (texto_status.includes("canais_repl"))
+        texto_status = texto_status.replace("canais_repl", client.channels(0).size)
 
-        if (texto_status.includes("canais_repl"))
-            texto_status = texto_status.replace("canais_repl", client.channels(0).size)
+    if (texto_status.includes("activities_repl"))
+        texto_status = texto_status.replace("activities_repl", activities.length)
 
-        if (texto_status.includes("activities_repl"))
-            texto_status = texto_status.replace("activities_repl", activities.length)
+    if (texto_status.includes("version_repl")) {
+        const bot = await client.getBot()
+        texto_status = texto_status.replace("version_repl", bot.persis.version)
+    }
 
-        if (texto_status.includes("version_repl")) {
-            const bot = await client.getBot()
-            texto_status = texto_status.replace("version_repl", bot.persis.version)
-        }
+    if (texto_status.includes("commands_repl")) {
+        const bot = await client.getBot()
+        texto_status = texto_status.replace("commands_repl", bot.persis.commands)
+    }
 
-        if (texto_status.includes("commands_repl")) {
-            const bot = await client.getBot()
-            texto_status = texto_status.replace("commands_repl", bot.persis.commands)
-        }
+    // Exibindo o status personalizado de forma aleatória por um tempo
+    client.user().setActivity(texto_status, { type: actionTypes[activities[num].type] })
+    client.cached.presence = num // Registrando o número do status atual
 
-        // Exibindo o status personalizado de forma aleatória por um tempo
-        client.user().setActivity(texto_status, { type: actionTypes[activities[num].type] })
-        client.cached.presence = num // Registrando o número do status atual
-
+    // Solicitando um novo status personalizado
+    timeout_presence = setTimeout(() => {
         requisita_status(client)
     }, 15000 + client.random(5000, tempo_minimo))
 }
