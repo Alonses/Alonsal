171b3ec2ac5d82264ce00ba584708e9486bd8f13
odnols <gusbra21@outlook.com>
2023-10-21 23:22:35 -0300

    guild?.conf.logger

--- core/interactions/chunks/panel_guild_free_games.js
+++ core/interactions/chunks/panel_guild_free_games.js
@@ -33,7 +33,7 @@ module.exports = async ({ client, user, interaction }) => {
         })
 
     botoes = botoes.concat([
-        { id: "guild_free_games_button", name: client.tls.phrase(user, "manu.painel.anuncio_games"), type: type_button(guild?.conf.logger), emoji: emoji_button(guild?.conf.logger), data: "1" },
+        { id: "guild_free_games_button", name: client.tls.phrase(user, "manu.painel.anuncio_games"), type: type_button(guild?.conf.games), emoji: emoji_button(guild?.conf.games), data: "1" },
         { id: "guild_free_games_button", name: client.tls.phrase(user, "menu.botoes.anunciar_agora"), type: 1, emoji: client.emoji(6), data: "2" },
         { id: "guild_free_games_button", name: client.tls.phrase(user, "mode.anuncio.cargo"), type: 1, emoji: client.defaultEmoji("role"), data: "3" },
         { id: "guild_free_games_button", name: client.tls.phrase(user, "mode.report.canal_de_avisos"), type: 1, emoji: client.defaultEmoji("channel"), data: "4" }
