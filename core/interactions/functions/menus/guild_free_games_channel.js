2dbbd73bc9745b4b842301b1da7ed28c4bb35367
odnols <gusbra21@outlook.com>
2023-10-21 23:20:45 -0300

    guild.game.role

--- core/interactions/functions/menus/guild_free_games_channel.js
+++ core/interactions/functions/menus/guild_free_games_channel.js
@@ -5,7 +5,7 @@ module.exports = async ({ client, user, interaction, dados }) => {
 
     await guild.save()
 
-    if (!guild.game.role) { // Redirecionando para escolher o cargo
+    if (!guild.games.role) { // Redirecionando para escolher o cargo
         dados = `${interaction.user.id}.3`
         return require('../buttons/guild_free_games_button')({ client, user, interaction, dados })
     }
