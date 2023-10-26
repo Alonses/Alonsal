d244ac722d4e2a1b74579c20c66dd7087e19ce16
odnols <gustavobrandao31@outlook.com>
2023-10-26 19:52:17 -0300

    Refatorando ceiras

--- core/interactions/functions/buttons/user_panel_button.js
+++ core/interactions/functions/buttons/user_panel_button.js
@@ -1,7 +1,16 @@
+const operations = {
+    1: ["conf", "ghost_mode"],
+    2: ["conf", "notify"],
+    3: ["conf", "ranking"],
+    4: ["conf", "public_badges"],
+    5: ["misc", "weather"],
+    6: ["conf", "global_tasks"]
+}
+
 module.exports = async ({ client, user, interaction, dados }) => {
 
     const escolha = parseInt(dados.split(".")[1])
-    let pagina = 0
+    let operador = 0
 
     // Tratamento dos cliques
     // 1 -> Modo fantasma
@@ -12,54 +21,13 @@ module.exports = async ({ client, user, interaction, dados }) => {
     // 5 -> Clima resumido
     // 6 -> Tarefas globais
 
-    if (escolha === 1) {
-        // Ativa ou desativa o modo fantasma
-        if (typeof user.conf.ghost_mode !== "undefined")
-            user.conf.ghost_mode = !user.conf.ghost_mode
-        else
-            user.conf.ghost_mode = true
-
-    } else if (escolha === 2) {
-
-        // Ativa ou desativa as notificações em DM
-        if (typeof user.conf.notify !== "undefined")
-            user.conf.notify = !user.conf.notify
-        else
-            user.conf.notify = false
-    } else if (escolha === 3) {
-
-        // Ativa ou desativa o ranking do usuário
-        if (typeof user.conf.ranking !== "undefined")
-            user.conf.ranking = !user.conf.ranking
-        else
-            user.conf.ranking = false
-    } else if (escolha === 4) {
-
-        // Ativa ou desativa a exibição de badges publicamente
-        if (typeof user.conf.public_badges !== "undefined")
-            user.conf.public_badges = !user.conf.public_badges
-        else
-            user.conf.public_badges = false
-    } else if (escolha === 5) {
-
-        // Ativa ou desativa a versão resumida dos retornos de clima
-        if (typeof user.misc.weather !== "undefined")
-            user.misc.weather = !user.misc.weather
-        else
-            user.misc.weather = true
-    } else if (escolha === 6) {
-
-        // Ativa ou desativa as tarefas globais
-        if (typeof user.conf.global_tasks !== "undefined")
-            user.conf.global_tasks = !user.conf.global_tasks
-        else
-            user.conf.global_tasks = true
-    }
+    // Ativa ou desativa a operação selecionada
+    user[operations[escolha][0]][operations[escolha][1]] = !user[operations[escolha][0]][operations[escolha][1]]
 
     if (escolha > 3)
-        pagina = 1
+        operador = 1
 
     await user.save()
 
-    require('../../chunks/panel_personal')({ client, user, interaction, pagina })
+    require('../../chunks/panel_personal')({ client, user, interaction, operador })
 }
