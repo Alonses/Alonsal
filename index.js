5492882ec99676ec67e735b34143368176134c16
odnols <gustavobrandao31@outlook.com>
2023-12-11 22:48:34 -0300

    syncando

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const { ShardingManager } = require('discord.js')
 const { client_data } = require('./setup')
 
+console.clear() // Limpando o console e inicializando o bot
+
 const shard_names = require('./files/json/text/shard_names.json')
 
 const manager = new ShardingManager('./bot.js', { token: client_data.token })
-
-console.clear() // Limpando o console e inicializando o bot
-
 manager.on('shardCreate', shard => console.log(`💠 | Shard ${shard_names[shard.id]} ativado`))
 
 manager.spawn()
