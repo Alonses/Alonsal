a70295d8a3d355ff019460855333934ed53462bd
odnols <gusbra21@outlook.com>
2024-02-03 21:04:47 -0300

    ajustes no /mine

---
+++ commands/utilities/subcommands/mine_chunk.js
@@ -0,0 +1,43 @@
+const { EmbedBuilder } = require('discord.js')
+
+module.exports = async ({ client, user, interaction }) => {
+
+    const x = interaction.options.getInteger("x")
+    const z = interaction.options.getInteger("z")
+
+    const x_chunk = Math.abs(x % 16)
+    const z_chunk = Math.abs(z % 16)
+
+    const chunk = []
+
+    let i = x < 0 ? 15 : 0
+    let final = i === 15 ? 0 : 16
+
+    while (i < final) {
+
+        let linha = ""
+        k = z < 0 ? 15 : 0
+
+        while (k < final) {
+            if (x_chunk === i && z_chunk === k)
+                linha += "x "
+            else
+                linha += "- "
+
+            k++
+        }
+
+        chunk.push(linha)
+        i++
+    }
+
+    const embed = new EmbedBuilder()
+        .setTitle("> Sua posição na chunk")
+        .setColor(client.embed_color(user.misc.color))
+        .setDescription(`${client.defaultEmoji("earth")} **Coordenadas:** X: \`${x}\`, Z: \`${z}\`\n\n**Dentro da chunk:** X: \`${x_chunk}\`, Z: \`${z_chunk}\`\n\`\`\`${chunk.join("\n")}\`\`\``)
+
+    interaction.reply({
+        embeds: [embed],
+        ephemeral: client.decider(user?.conf.ghost_mode, 0)
+    })
+}
---
+++ commands/utilities/subcommands/mine_portal.js
@@ -0,0 +1,39 @@
+const { EmbedBuilder } = require('discord.js')
+
+module.exports = async ({ client, user, interaction }) => {
+
+    let x = interaction.options.getInteger("x")
+    let z = interaction.options.getInteger("z")
+
+    // Do nether para a superfície ( *8 )
+    let operacao = 0, caso = `🍀 ${client.tls.phrase(user, "game.portal.superficie")} -> 🔥 Nether`, dimension = client.tls.phrase(user, "game.portal.na_superficie"), to_dimension = client.tls.phrase(user, "game.portal.o_nether")
+
+    if (interaction.options.getString("destiny"))
+        operacao = parseInt(interaction.options.getString("destiny"))
+
+    if (operacao === 0) { // Para a superfície
+        x /= 8, z /= 8
+
+        caso = `🔥 Nether -> 🍀 ${client.tls.phrase(user, "game.portal.superficie")}`, dimension = client.tls.phrase(user, "game.portal.no_nether"), to_dimension = client.tls.phrase(user, "game.portal.a_superficie")
+    } else
+        x *= 8, z *= 8
+
+    let x_i = parseInt(x), z_i = parseInt(z)
+
+    x = interaction.options.getInteger("x")
+    z = interaction.options.getInteger("z")
+
+    const embed = new EmbedBuilder()
+        .setTitle(client.tls.phrase(user, "game.portal.titulo"))
+        .setColor(client.embed_color(user.misc.color))
+        .setDescription(client.replace(client.tls.phrase(user, "game.portal.descricao"), [client.emoji("mc_portal"), dimension, x_i, z_i, client.emoji("mc_portal_frame"), to_dimension, x, z]))
+        .setFooter({
+            text: caso,
+            iconURL: interaction.user.avatarURL({ dynamic: true })
+        })
+
+    interaction.reply({
+        embeds: [embed],
+        ephemeral: client.decider(user?.conf.ghost_mode, 0)
+    })
+}
