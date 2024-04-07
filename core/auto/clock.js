88a341a9a08bae3d0f2acaf7720e0ca905e7eace
odnols <gusbra21@outlook.com>
2024-04-07 13:10:24 -0300

    Ranking global e ajustes

--- core/auto/clock.js
+++ core/auto/clock.js
@@ -1,6 +1,7 @@
 const { verifica_warns } = require("./warn")
-const { requisita_modulo } = require("./module")
 const { verifica_eraser } = require("./guild_eraser")
+const { requisita_modulo } = require("./module")
+const { verifica_servers } = require("../data/ranking")
 const { verifica_user_eraser } = require("./user_eraser")
 
 const sync_dynamic_badges = require("./dynamic_badges")
@@ -21,13 +22,16 @@ internal_clock = (client, tempo_restante) => {
 
         // Verificando se há modulos para o horário atual
         requisita_modulo()
+        verifica_warns(client) // Sincronizando as advertências que se expirão
 
         if (client.timestamp() % 600 < 60) {
             sync_dynamic_badges(client) // Sincronizando as badges que são dinâmicas
-            verifica_warns(client) // Sincronizando as advertências que se expirão
             verifica_eraser(client) // Verificando se há dados de servidores que se expiraram
             verifica_user_eraser(client) // Verificando se há dados de usuários que se expiraram
         }
+
+        if (client.timestamp() % 1800 < 60)
+            verifica_servers() // Sincronizando o ranking global dos usuários que ganharam XP
 
         internal_clock(client, 60000)
     }, tempo_restante)
