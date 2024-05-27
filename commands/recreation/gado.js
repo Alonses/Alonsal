7bb556c300552d96ed970a778c4c268e258e3f03
odnols <gustavobrandao31@outlook.com>
2024-05-26 22:10:08 -0300

    Canvas com o /gado e ajustes

--- commands/recreation/gado.js
+++ commands/recreation/gado.js
@@ -1,4 +1,6 @@
-const { SlashCommandBuilder, PermissionsBitField } = require('discord.js')
+const { SlashCommandBuilder, PermissionsBitField, AttachmentBuilder } = require('discord.js')
+
+const Canvas = require('@napi-rs/canvas')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -46,6 +48,32 @@ module.exports = {
         if (!await client.permissions(null, client.id(), [PermissionsBitField.Flags.ViewChannel, PermissionsBitField.Flags.SendMessages], interaction))
             return client.tls.reply(interaction, user, "dive.gado.permissao", true, 7)
 
-        interaction.reply({ content: client.tls.phrase(user, "dive.gado.frases", null, `<@${alvo.id}>`) })
+        await interaction.deferReply({ ephemeral: true })
+
+        let frase_gado = client.tls.phrase(user, "dive.gado.frases", null, `\`${alvo.username}\``)
+        const { gado } = require('../../files/json/text/gado.json')
+
+        const operador = frase_gado.split("|")[0] || "0"
+        let url_img = gado[operador][client.random(gado[operador])]
+        frase_gado = frase_gado.split("|")[1]
+
+        if (process.env.id_bool.includes(alvo.id)) url_img = "https://i.imgur.com/jtOMyfJ.png"
+
+        const image = new AttachmentBuilder(alvo.avatarURL({ dynamic: true }), "img.png")
+        let canvas = Canvas.createCanvas(300, 300)
+        const context = canvas.getContext('2d')
+
+        // Carregando as imagens de perfil do usuário
+        const sprite_user = await Canvas.loadImage(alvo.avatarURL({ dynamic: true }) || "https://archive.org/download/discordprofilepictures/discordred.png")
+        const sprite_chifre = await Canvas.loadImage(url_img)
+
+        // Desenhando no canvas
+        context.drawImage(sprite_user, 0, 0, 270, 270)
+        context.drawImage(sprite_chifre, 0, 0, 300, 300)
+
+        // Gerando a imagem para poder anexar ao canvas
+        attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'new_avatar.png' })
+
+        interaction.editReply({ content: frase_gado, files: [attachment], ephemeral: true })
     }
 }
