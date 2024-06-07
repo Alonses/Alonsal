ecced3a335c40baa81e0ae4ecf5f54a24e911606
odnols <gusbra21@outlook.com>
2024-06-07 19:52:17 -0300

    Mongodb

--- core/database/database.js
+++ core/database/database.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose")
 
 async function setup(uri) {
-    mongoose.set('strictQuery', true)
-    await mongoose.connect(uri)
+    mongoose.set('strictQuery', false)
+    mongoose.connect(uri)
 
+    console.log("🟣 | Banco conectado")
     var database = mongoose.connection
 }
 
-module.exports = {
-    setup
-}
+module.exports.setup = setup
