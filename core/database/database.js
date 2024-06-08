b94c5d79597fb448c281973e6590a69eb5332f12
odnols <gustavobrandao31@outlook.com>
2024-06-08 10:49:03 -0300

    Mongodb

--- core/database/database.js
+++ core/database/database.js
@@ -3,9 +3,16 @@ const mongoose = require("mongoose")
 async function setup(uri) {
     mongoose.set('strictQuery', false)
     mongoose.connect(uri)
+        .catch(error => handleError(error));
 
     console.log("🟣 | Banco conectado")
     var database = mongoose.connection
 }
+
+mongoose.connection.on('error', err => {
+    console.log("🔴 | Erro com o Banco")
+
+    console.log(err)
+})
 
 module.exports.setup = setup
