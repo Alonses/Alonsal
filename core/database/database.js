857dedb2e3bca02afe8edc1eb415d8609e4e62a6
odnols <gustavobrandao31@outlook.com>
2024-06-30 18:49:16 -0300

    removendo handleError

--- core/database/database.js
+++ core/database/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 async function setup(uri) {
     mongoose.set('strictQuery', false)
     mongoose.connect(uri)
-        .catch(error => handleError(error));
+        .catch(error => console.log(error));
 
     console.log("🟣 | Banco conectado")
     var database = mongoose.connection
