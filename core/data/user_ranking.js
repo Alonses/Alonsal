de51dd03f54073ed8f7386bd6b99dd8ba9c0e2ab
odnols <gustavobrandao31@outlook.com>
2025-01-04 18:55:31 -0300

    Ajustes com ranking global e ephemerals

--- core/data/user_ranking.js
+++ core/data/user_ranking.js
@@ -11,25 +11,12 @@ module.exports = async ({ client, message, caso }) => {
 
     //            Comandos            Mensagens
     let id_alvo = message.user?.id || message.author?.id
+    let cached_erase = false
 
-    // Ignora a interação caso não tenha sido acionado em um servidor
-    if (!message.guild) return
-
-    // Coletando os dados do usuário alvo
-    let user = await getUserRankServer(client.encrypt(id_alvo), client.encrypt(message.guild.id))
-
-    // Sincronizando o XP interno de todos os servidores que o usuário faz parte
-    if (!user.ixp) {
-        user.ixp = user.xp
-        await sincroniza_xp(user)
-    }
-
-    const user_data = await client.getUser(user.uid) // Salvando a última interação do usuário
+    const user_data = await client.getUser(id_alvo) // Salvando a última interação do usuário
 
     if (user_data.erase.forced)
         return // Usuário forçou a exclusão de dados
-
-    let cached_erase = false
 
     if (user_data.erase.valid) { // Usuário interagiu com o Alonsal novamente
         client.sendDM(user_data, { content: client.tls.phrase(user_data, "manu.data.aviso_remocao_exclusao", client.defaultEmoji("person")) })
@@ -38,40 +25,56 @@ module.exports = async ({ client, message, caso }) => {
         cached_erase = true
     }
 
-    if (user.erase.valid) { // Usuário interagiu com o Alonsal novamente
+    user_data.erase.erase_on = client.timestamp() + defaultUserEraser[user_data.erase.timeout]
+
+    // Ignora a interação caso não tenha sido acionado em um servidor
+    if (!message.guild) {
+
+        await user_data.save()
+        return
+    }
+
+    // Coletando os dados do usuário alvo
+    let guild_user = await getUserRankServer(client.encrypt(id_alvo), client.encrypt(message.guild.id))
+
+    if (guild_user.erase.valid) { // Usuário interagiu com o Alonsal novamente
         client.sendDM(user_data, { content: client.tls.phrase(user_data, "manu.data.aviso_remocao_exclusao_servidor", client.defaultEmoji("person"), await (client.guilds(message.guild.id)).name) })
 
-        user.erase.valid = false // Retirando a etiqueta para remoção de dados
+        guild_user.erase.valid = false // Retirando a etiqueta para remoção de dados
         cached_erase = true
     }
 
-    user_data.erase.erase_on = client.timestamp() + defaultUserEraser[user_data.erase.timeout]
+    // Sincronizando o XP interno de todos os servidores que o usuário faz parte
+    if (!guild_user.ixp) {
+        guild_user.ixp = guild_user.xp
+        await sincroniza_xp(guild_user)
+    }
 
     if (cached_erase) // Atualizando a lista de usuários que estão marcados para exclusão
         atualiza_user_eraser(client)
 
     // Validando se o usuário tem o ranking habilitado
-    if (!await client.verifyUserRanking(user.uid)) return
+    if (!await client.verifyUserRanking(guild_user.uid)) return
 
     //              Comandos                  Mensagens
-    user.nickname = message.user?.username || message.author?.username
+    guild_user.nickname = message.user?.username || message.author?.username
 
     if (caso === "messages") {
-        if (user.warns >= CHECKS.LIMIT) {
-            user.caldeira_de_ceira = true
-            user.warns = 0
+        if (guild_user.warns >= CHECKS.LIMIT) {
+            guild_user.caldeira_de_ceira = true
+            guild_user.warns = 0
 
             validador = true
-            await user.save()
+            await guild_user.save()
             await user_data.save()
 
             return
         }
 
-        if (message.createdTimestamp - user.lastInteraction < CHECKS.DIFF) {
-            user.warns++
+        if (message.createdTimestamp - guild_user.lastInteraction < CHECKS.DIFF) {
+            guild_user.warns++
 
-            await user.save()
+            await guild_user.save()
             await user_data.save()
 
             return
@@ -79,37 +82,37 @@ module.exports = async ({ client, message, caso }) => {
     }
 
     // Limitando o ganho de XP por spam no chat
-    if (user.caldeira_de_ceira)
-        if (message.createdTimestamp - user.lastInteraction > CHECKS.HOLD)
-            user.caldeira_de_ceira = false
+    if (guild_user.caldeira_de_ceira)
+        if (message.createdTimestamp - guild_user.lastInteraction > CHECKS.HOLD)
+            guild_user.caldeira_de_ceira = false
         else if (caso === "messages") return
 
     // Coletando o XP atual e somando ao total do usuário
-    let xp_anterior = user.ixp
+    let xp_anterior = guild_user.ixp
 
     // Recalculando o tempo de inatividade do usuário
-    user.erase.erase_on = client.timestamp() + defaultUserEraser[user_data.erase.guild_timeout]
+    guild_user.erase.erase_on = client.timestamp() + defaultUserEraser[user_data.erase.guild_timeout]
 
     // Verificando se o servidor da interação está com o ranking habilitado
     if (client.cached.ranked_guilds.has(client.encrypt(message.guild.id))) {
         if (caso === "messages") {
 
-            user.xp += client.cached.ranking_value
-            user.ixp += client.cached.ranking_value
+            guild_user.xp += client.cached.ranking_value
+            guild_user.ixp += client.cached.ranking_value
 
-            user.lastInteraction = message.createdTimestamp
-            user.warns = 0
+            guild_user.lastInteraction = message.createdTimestamp
+            guild_user.warns = 0
         } else if (caso === "comando") { // Experiência obtida executando comandos
-            user.xp += client.cached.ranking_value * 1.5
-            user.ixp += client.cached.ranking_value * 1.5
+            guild_user.xp += client.cached.ranking_value * 1.5
+            guild_user.ixp += client.cached.ranking_value * 1.5
         } else { // Experiência obtida ao usar botões ou menus
-            user.xp += client.cached.ranking_value * 0.5
-            user.ixp += client.cached.ranking_value * 0.5
+            guild_user.xp += client.cached.ranking_value * 0.5
+            guild_user.ixp += client.cached.ranking_value * 0.5
         }
     }
 
     // Bônus em Bufunfas por subir de nível
-    if (parseInt(user.ixp / 1000) !== parseInt(xp_anterior / 1000)) {
+    if (parseInt(guild_user.ixp / 1000) !== parseInt(xp_anterior / 1000)) {
 
         user_data.misc.money += 250
         await user_data.save()
@@ -121,15 +124,15 @@ module.exports = async ({ client, message, caso }) => {
 
     // Registrando no relatório algumas informações
     client.journal(caso)
-    await user.save()
+    await guild_user.save()
 
     // Adicionando o usuário na fila de ranking global para a próxima sincronização
-    if (!members_xp.includes(user.uid)) members_xp.push(user.uid)
+    if (!members_xp.includes(guild_user.uid)) members_xp.push(guild_user.uid)
 }
 
-sincroniza_xp = async (user) => {
+sincroniza_xp = async (guild_user) => {
 
-    const servidores = await getUserRankServers(user.uid)
+    const servidores = await getUserRankServers(guild_user.uid)
 
     servidores.forEach(async servidor => {
         servidor.ixp = servidor.xp
@@ -153,16 +156,19 @@ async function verifica_servers() {
             const servers = await getUserRankServers(id_user)
             let user_global = await getUserGlobalRank(id_user), maior = 0
 
-            servers.forEach(async servidor => {
-                if (servidor.ixp > maior) {
-                    maior = servidor.ixp
+            if (servers) {
+                servers.forEach(async servidor => {
+                    if (servidor.ixp > maior) {
+                        maior = servidor.ixp
 
-                    user_global.xp = servidor.ixp
-                    user_global.sid = servidor.sid
-                }
-            })
+                        user_global.xp = servidor.ixp
+                        user_global.sid = servidor.sid
+                    }
+                })
 
-            await user_global.save()
+                await user_global.save()
+            }
+
             array_copia.shift()
         }
 }
